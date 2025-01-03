4b70624fbb86b7e475246522306c9586c6e0f283
mm <mm@sofa.local>
2025-01-03 04:54:23 +0100

    modified:   .gitignore deleted:    notes/gadient_hex_rgb.py modified:   src/chezmoi_mousse/__init__.py modified:   src/chezmoi_mousse/blocks.py modified:   src/chezmoi_mousse/operate.py modified:   src/chezmoi_mousse/start.py modified:   src/chezmoi_mousse/tui.py

--- src/chezmoi_mousse/__init__.py
+++ src/chezmoi_mousse/__init__.py
@@ -1,8 +1,1 @@
 """ The root chezmoi-mousse module."""
-
-from chezmoi_mousse.operate import ChezmoiCommands
-
-CM_CONFIG_DUMP = ChezmoiCommands().dump_config()
-CM_DATA = ChezmoiCommands().data()
-CM_DOCTOR = ChezmoiCommands().doctor()
-CM_CONFIG_CAT = ChezmoiCommands().cat_config()
