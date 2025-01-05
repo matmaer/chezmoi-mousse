f39e0b949f4d57f52eec7df39804445a8632a99e
mm <mm@sofa.local>
2025-01-05 22:35:45 +0100

    make richlog for stdout more pretty and prepare for operations to log all stdout for reference to the richlog sidebar modified:   src/chezmoi_mousse/__init__.py modified:   src/chezmoi_mousse/tui.py modified:   src/chezmoi_mousse/tui.tcss

--- src/chezmoi_mousse/__init__.py
+++ src/chezmoi_mousse/__init__.py
@@ -1,5 +1,18 @@
 """The root chezmoi-mousse module."""
 
+from textual.app import ComposeResult, Widget
+from textual.widgets import RichLog
 from chezmoi_mousse.operate import ChezmoiCommands
 
+
 chezmoi = ChezmoiCommands()
+
+
+class RichLogSidebar(Widget):
+    def compose(self) -> ComposeResult:
+        yield RichLog(
+            id="richlog",
+            highlight=True,
+            wrap=False,
+            markup=True,
+        )
