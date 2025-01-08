383f3ff62c82e8ffdc359237f0aa521265444eb8
mm <mm@sofa.local>
2025-01-08 15:26:06 +0100

    modified:   src/chezmoi_mousse/__init__.py modified:   src/chezmoi_mousse/greeter.py modified:   src/chezmoi_mousse/text_blocks.py

--- src/chezmoi_mousse/__init__.py
+++ src/chezmoi_mousse/__init__.py
@@ -1,22 +1,5 @@
 """The root chezmoi-mousse module."""
 
-# from textual.widgets import RichLog
-
 from chezmoi_mousse.commands import ChezmoiCommands
-# from chezmoi_mousse.logger import MousseLogger
 
 chezmoi = ChezmoiCommands()
-
-
-# rlog = MousseLogger()
-
-# class MousseLogger(RichLog):
-#     def __init__(self):
-#         super().__init__(
-#             id="monlog",
-#             highlight=True,
-#             wrap=False,
-#             markup=True,
-#         )
-#     def compose(self):
-#         yield MousseLogger()
