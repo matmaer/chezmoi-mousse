e03c7fc4c07ba58fa263b0cbd77a75daf8980cc0
mm <mm@sofa.local>
2025-01-18 00:58:59 +0100

    added loadingindicator mdified:   src/chezmoi_mousse/inspector.py

--- src/chezmoi_mousse/inspector.py
+++ src/chezmoi_mousse/inspector.py
@@ -7,6 +7,7 @@ from textual import work
 from textual.widgets import (
     DataTable,
     Footer,
+    LoadingIndicator,
     Pretty,
     Static,
     TabbedContent,
@@ -18,16 +19,17 @@ from chezmoi_mousse.page import PageScreen
 
 class ChezmoiDoctor(Static):
     def compose(self) -> ComposeResult:
-        yield DataTable()
+        yield DataTable(id="doctor")
+        yield LoadingIndicator()
 
     def on_mount(self):
-        data_table = self.query_one(DataTable)
+        data_table = self.query_one("#doctor")
         data_table.loading = True
         self.construct_table()
 
     @work(thread=True)
     def construct_table(self) -> None:
-        data_table = self.query_one(DataTable)
+        data_table = self.query_one("#doctor")
         cm_dr_output = chezmoi.doctor()
         data_table.cursor_type = "row"
         header_row = cm_dr_output.pop(0).split()
