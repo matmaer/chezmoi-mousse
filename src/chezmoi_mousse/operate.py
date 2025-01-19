f5dba5125e37c94ee7f3b66715ae67e5f13b7007
mm <mm@sofa.local>
2025-01-19 01:34:05 +0100

    Formatting status output. Adding command outputs. modified:   .vscode/tasks.json modified:   src/chezmoi_mousse/commands.py deleted:    src/chezmoi_mousse/debug.py modified:   src/chezmoi_mousse/inspector.py modified:   src/chezmoi_mousse/operate.py modified:   src/chezmoi_mousse/page.py modified:   src/chezmoi_mousse/tui.py modified:   src/chezmoi_mousse/tui.tcss

--- src/chezmoi_mousse/operate.py
+++ src/chezmoi_mousse/operate.py
@@ -3,9 +3,18 @@
 from pathlib import Path
 from typing import Iterable
 
+from textual.app import ComposeResult
 from textual.widget import Widget
 from textual.containers import Center
-from textual.widgets import Footer, TabbedContent, DirectoryTree, Pretty, Static
+from textual.widgets import (
+    Footer,
+    TabbedContent,
+    DirectoryTree,
+    Label,
+    Static,
+    DataTable,
+    LoadingIndicator,
+)
 from chezmoi_mousse import chezmoi
 from chezmoi_mousse.page import PageScreen
 
@@ -18,6 +27,7 @@ VISUAL_DIAGRAM = """\
 └──────┬───────┘    └──────┬───────┘    └──────┬───────┘    └──────┬───────┘
        │                   │                   │                   │
        │    chezmoi add    │                   │                   │
+       │   chezmoi re-add  │                   │                   │
        │──────────────────>│                   │                   │
        │                   │                   │                   │
        │   chezmoi apply   │                   │                   │
@@ -25,7 +35,7 @@ VISUAL_DIAGRAM = """\
        │                   │                   │                   │
        │  chezmoi status   │                   │                   │
        │   chezmoi diff    │                   │                   │
-       │<─ ─ ─ ─ ─ ─ ─ ─ ─ │                   │     git push      │
+       │<─ ─ ─ ─ ─ ─ ─ ─ ─>│                   │     git push      │
        │                   │                   │──────────────────>│
        │                   │                   │                   │
        │                   │           chezmoi git pull            │
@@ -66,6 +76,73 @@ class ManagedFiles(DirectoryTree):
         return [path for path in paths if path in self.managed]
 
 
+class ChezmoiStatus(Static):
+    status_meaning = {
+        "space": {
+            "Status": "No change",
+            "Re_Add_Change": "No change",
+            "Apply_Change": "No change",
+        },
+        "A": {
+            "Status": "Added",
+            "Re_Add_Change": "Entry was created",
+            "Apply_Change": "Entry will be created",
+        },
+        "D": {
+            "Status": "Deleted",
+            "Re_Add_Change": "Entry was deleted",
+            "Apply_Change": "Entry will be deleted",
+        },
+        "M": {
+            "Status": "Modified",
+            "Re_Add_Change": "Entry was modified",
+            "Apply_Change": "Entry will be modified",
+        },
+        "R": {
+            "Status": "Run",
+            "Re_Add_Change": "Not applicable",
+            "Apply_Change": "Entry will be run",
+        },
+    }
+
+    def __init__(self):
+        super().__init__()
+        self.status_output = chezmoi.status()
+
+    def compose(self) -> ComposeResult:
+        yield Label("Chezmoi Apply Status", variant="primary")
+        yield DataTable(id="apply_table")
+        yield Label("Chezmoi Re-Add Status", variant="primary")
+        yield DataTable(id="re_add_table")
+        yield LoadingIndicator()
+
+    def on_mount(self):
+        re_add_table = self.query_one("#re_add_table")
+        apply_table = self.query_one("#apply_table")
+
+        # FIRST COLUMN: difference between the last state written by chezmoi and the actual state
+        # SECOND COLUMN: difference between the actual state and the target state, and what effect running chezmoi apply will have
+        header_row = ["STATUS", "PATH", "CHANGE"]
+
+        re_add_table.add_columns(*header_row)
+        apply_table.add_columns(*header_row)
+
+        for line in self.status_output:
+            path = line[3:]
+
+            apply_status = self.status_meaning[line[0]]["Status"]
+            apply_change = self.status_meaning[line[0]]["Re_Add_Change"]
+
+            re_add_status = self.status_meaning[line[1]]["Status"]
+            re_add_change = self.status_meaning[line[1]]["Apply_Change"]
+
+            apply_row = [apply_status, path, apply_change]
+            apply_table.add_row(*apply_row)
+
+            re_add_row = [re_add_status, path, re_add_change]
+            re_add_table.add_row(*re_add_row)
+
+
 class OperationTabs(PageScreen):
     def compose(self):
         with TabbedContent(
@@ -75,5 +152,5 @@ class OperationTabs(PageScreen):
         ):
             yield Static(VISUAL_DIAGRAM)
             yield ManagedFiles()
-            yield Pretty(chezmoi.status())
+            yield ChezmoiStatus()
         yield Footer()
--- src/chezmoi_mousse/page.py
+++ src/chezmoi_mousse/page.py
@@ -32,7 +32,7 @@ from textual.app import ComposeResult
 from textual.binding import Binding
 from textual.containers import ScrollableContainer
 from textual.screen import ModalScreen, Screen
-from textual.widgets import Static
+from textual.widgets import Static, Label, Pretty
 
 
 class CodeScreen(ModalScreen):
@@ -48,7 +48,7 @@ class CodeScreen(ModalScreen):
         }
     }
     """
-    BINDINGS = [("escape", "dismiss", "Dismiss code")]
+    BINDINGS = [("escape", "dismiss", "Dismiss")]
 
     def __init__(self, title: str, code: str) -> None:
         super().__init__()
@@ -70,21 +70,91 @@ class CodeScreen(ModalScreen):
         code_widget.border_subtitle = "Escape to close"
 
 
-class PageScreen(Screen):
+class VariablesScreen(ModalScreen):
     DEFAULT_CSS = """
-    PageScreen {
-        width: 100%;
-        height: 1fr;
-        overflow-y: auto;
+    VariablesScreen {
+        #vars {
+            border: heavy $accent;
+            margin: 2 4;
+            scrollbar-gutter: stable;
+            Static {
+                width: auto;
+            }
+        }
     }
     """
+    BINDINGS = [("escape", "dismiss", "Dismiss")]
+
+    def __init__(self, local_vars: dict, global_vars: dict) -> None:
+        super().__init__()
+        self.local_vars = local_vars
+        self.global_vars = global_vars
+        if "__builtins__" in self.global_vars:
+            del self.global_vars["__builtins__"]
+        if "__cached__" in self.global_vars:
+            del self.global_vars["__cached__"]
+
+    def compose(self) -> ComposeResult:
+        with ScrollableContainer(id="vars"):
+            yield Label("Global Variables", variant="primary")
+            yield Pretty(self.global_vars)
+            yield Label("Local Variables", variant="primary")
+            yield Pretty(self.local_vars)
+
+    def on_mount(self):
+        code_widget = self.query_one("#vars")
+        code_widget.border_title = "Variables"
+        code_widget.border_subtitle = "Escape to close"
+
+
+class SettingsScreen(ModalScreen):
+    DEFAULT_CSS = """
+    SettingsScreen {
+        #settings {
+            border: heavy $accent;
+            margin: 2 4;
+            scrollbar-gutter: stable;
+            Static {
+                width: auto;
+            }
+        }
+    }
+    """
+    BINDINGS = [("escape", "dismiss", "Dismiss")]
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    def compose(self) -> ComposeResult:
+        with ScrollableContainer(id="settings"):
+            yield Label("chezmoi-mousse settings", variant="primary")
+
+    def on_mount(self):
+        code_widget = self.query_one("#settings")
+        code_widget.border_title = "chezmoi-mousse settings"
+        code_widget.border_subtitle = "Escape to close"
+
+
+class PageScreen(Screen):
     BINDINGS = [
         Binding(
             "c",
             "show_code",
             "Code",
             tooltip="Show the code used to generate this screen",
-        )
+        ),
+        Binding(
+            "v",
+            "show_vars",
+            "Variables",
+            tooltip="Show the local and global variables",
+        ),
+        Binding(
+            "s",
+            "show_settings",
+            "Settings",
+            tooltip="chezmoi-mousse settings",
+        ),
     ]
 
     @work(thread=True)
@@ -115,3 +185,11 @@ class PageScreen(Screen):
             )
         else:
             self.app.push_screen(CodeScreen("Code for this page", code))
+
+    async def action_show_vars(self):
+        local_vars = locals()
+        global_vars = globals()
+        self.app.push_screen(VariablesScreen(local_vars, global_vars))
+
+    async def action_show_settings(self):
+        self.app.push_screen(SettingsScreen())
