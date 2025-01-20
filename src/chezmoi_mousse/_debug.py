23209b2449ceda3687eb718cdf07e76872e93577
mm <mm@sofa.local>
2025-01-20 16:00:24 +0100

    Improve debugging setup modified:   .vscode/tasks.json modified:   src/chezmoi_mousse/_debug.py

--- src/chezmoi_mousse/_debug.py
+++ src/chezmoi_mousse/_debug.py
@@ -3,6 +3,21 @@ from textual.containers import ScrollableContainer
 from textual.screen import ModalScreen
 from textual.widgets import Label, Pretty
 
+# `textual console`: the -x flag can be used to exclude one or more groups:
+
+# EVENT
+# DEBUG
+# INFO
+# WARNING
+# ERROR
+# PRINT
+# SYSTEM
+# LOGGING
+# WORKER
+
+# Multiple groups may be excluded, for example:
+# textual console -x SYSTEM -x EVENT -x DEBUG -x INFO
+
 
 class VariablesScreen(ModalScreen):
     DEFAULT_CSS = """
