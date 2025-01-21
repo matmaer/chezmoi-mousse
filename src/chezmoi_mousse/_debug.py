64f006a96d23c5e49a4fb5f5464d4391d4f16725
mm <mm@sofa.local>
2025-01-21 07:56:14 +0100

    modified:   src/chezmoi_mousse/_debug.py

--- src/chezmoi_mousse/_debug.py
+++ src/chezmoi_mousse/_debug.py
@@ -1,3 +1,5 @@
+import os
+
 from textual.app import ComposeResult
 from textual.containers import ScrollableContainer
 from textual.screen import ModalScreen
@@ -17,6 +19,13 @@ from textual.widgets import Label, Pretty
 
 # Multiple groups may be excluded, for example:
 # textual console -x SYSTEM -x EVENT -x DEBUG -x INFO
+
+
+class DebugUtils:
+    @staticmethod
+    def print_env_vars():
+        for key, value in os.environ.items():
+            print(f"{key}: {value}")
 
 
 class VariablesScreen(ModalScreen):
@@ -45,3 +54,13 @@ class VariablesScreen(ModalScreen):
             yield Pretty(self.global_vars)
             yield Label("Local Variables", variant="primary")
             yield Pretty(self.local_vars)
+
+
+# logging message, example from Jazzhands:
+# @on(MyWidget.EventFoo)
+# async def cell_chosen(self, event: MyWidget.EventFoo):
+#     self.log.debug(
+#         f"event_foo: {event} \n"
+#         f"Row, col: {event.row}, {event.column}"    # example attributes in event
+#     )
+# handle event
