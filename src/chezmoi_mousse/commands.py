98ab628f95b49578eb0291ce21245528f1697f9a
mm <mm@sofa.local>
2025-01-21 07:50:57 +0100

    new file:   .vscode/launch.json modified:   .vscode/mousse.code-workspace modified:   src/chezmoi_mousse/commands.py

--- src/chezmoi_mousse/commands.py
+++ src/chezmoi_mousse/commands.py
@@ -43,13 +43,17 @@ class ChezmoiCommands:
         ]
 
     def _run(self, command: list) -> subprocess.CompletedProcess:
-        result = subprocess.run(
-            self.chezmoi + command,
-            capture_output=True,
-            encoding="utf-8",
-            shell=False,
-            timeout=2,  # temporary for development, should be one
-        )
+        try:
+            result = subprocess.run(
+                self.chezmoi + command,
+                capture_output=True,
+                encoding="utf-8",
+                shell=False,
+                timeout=2,  # temporary for development, should be one
+            )
+        except FileNotFoundError:
+            return "chezmoi not found"
+
         if result.returncode == 0:
             return result.stdout
         # chezmoi can be used without config file
@@ -58,23 +62,25 @@ class ChezmoiCommands:
         else:
             raise subprocess.CalledProcessError
 
-    def data(self) -> dict:
-        result = json.loads(self._run(["data", "--format=json"]))["chezmoi"]
-        if "args" in result.keys():
-            del result["args"]
-        return result
+    def data(self) -> dict | str:
+        result = self._run(["data", "--format=json"])
+        try:
+            result_dict = json.loads(result)
+            if "args" in result_dict.keys():
+                del result["args"]
+            return result_dict["chezmoi"]
+        except json.JSONDecodeError:
+            return result.strip()
 
-    def dump_config(self) -> dict:
-        return json.loads(self._run(["dump-config", "--format=json"]))
+    def dump_config(self) -> dict | str:
+        result = self._run(["dump-config", "--format=json"])
+        try:
+            return json.loads(result)
+        except json.JSONDecodeError:
+            return result.strip()
 
-    def target_state_dump(self) -> dict:
-        return json.loads(self._run(["dump"]))
-
-    def state_dump(self) -> dict:
-        return json.loads(self._run(["state", "dump"]))
-
-    def cat_config(self) -> dict:
-        result = self._run(["cat-config"])
+    def cat_config(self) -> dict | str:
+        result = self._run(["cat-config", "--format=json"])
         try:
             return tomllib.loads(result)
         except tomllib.TOMLDecodeError:
@@ -92,3 +98,17 @@ class ChezmoiCommands:
 
     def ignored(self) -> list:
         return self._run(["ignored"]).splitlines()
+
+    def target_state_dump(self) -> dict | str:
+        result = self._run(["dump"])
+        try:
+            return json.loads(result)
+        except json.JSONDecodeError:
+            return result.strip()
+
+    def state_dump(self) -> dict | str:
+        result = self._run(["state", "dump", "--format=json"])
+        try:
+            return json.loads(result)
+        except json.JSONDecodeError:
+            return result.strip()
