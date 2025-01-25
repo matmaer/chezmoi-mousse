522e46d92969c5011fa82b134de032bff921c853
mm <mm@sofa.local>
2025-01-25 23:48:38 +0100

    modified:   src/chezmoi_mousse/greeter.py 	modified:   src/chezmoi_mousse/tui.tcss

--- src/chezmoi_mousse/greeter.py
+++ src/chezmoi_mousse/greeter.py
@@ -1,13 +1,16 @@
 from collections import deque
 
+from time import sleep
 from rich.segment import Segment
 from rich.style import Style
+
+from textual import work
 from textual.app import ComposeResult
 from textual.binding import Binding
-from textual.containers import Center, Container, Middle
+from textual.containers import Container
 from textual.screen import Screen
 from textual.strip import Strip
-from textual.widgets import Footer, Label, ProgressBar, Static
+from textual.widgets import Footer, ProgressBar, Static, RichLog
 
 SPLASH = """\
  ██████╗██╗  ██╗███████╗███████╗███╗   ███╗ ██████╗ ██╗
@@ -54,17 +57,34 @@ class AnimatedFade(Static):
     """A widget to show a custom loading screen."""
 
     text = [line.ljust(len(max(SPLASH, key=len))) for line in SPLASH]
-    colors = deque([Style(color=color) for color in FADE])
+    line_styles = deque([Style(color=color) for color in FADE])
 
     def render_lines(self, crop) -> list[Strip]:
-        self.colors.rotate()
+        self.line_styles.rotate()
         return super().render_lines(crop)
 
     def render_line(self, y: int) -> Strip:
-        return Strip([Segment(self.text[y], style=self.colors[y])])
+        return Strip([Segment(self.text[y], style=self.line_styles[y])])
 
     def on_mount(self) -> None:
         self.set_interval(interval=0.04, callback=self.refresh, repeat=47)
+
+
+class CustomLoader(Static):
+    def compose(self):
+        yield ProgressBar()
+
+
+class ItemLoader(Static):
+    def compose(self):
+        yield RichLog()
+
+    @work(thread=True)
+    def on_mount(self) -> None:
+        for i in range(1, 6):
+            sleep(0.5)
+            # self.query_one(ProgressBar).advance(20)
+            self.query_one(RichLog).write(f"Loading items {i}")
 
 
 class LoadingScreen(Screen):
@@ -79,18 +99,13 @@ class LoadingScreen(Screen):
 
     def compose(self) -> ComposeResult:
         with Container(id="loadingscreen"):
-            yield Middle(
-                Center(AnimatedFade()),
-                Center(
-                    ProgressBar(
-                        total=100,
-                        gradient=FADE,
-                        show_eta=False,
-                        show_percentage=False,
-                    )
-                ),
-                Center(Label("Loading")),
-            )
-            # yield ItemLoader()
-            # yield Label("aoeu")
+            yield AnimatedFade()
+            yield ProgressBar()
+            yield ItemLoader()
         yield Footer()
+
+    @work(thread=True)
+    def on_mount(self) -> None:
+        for i in range(1, 6):
+            sleep(0.5)
+            self.query_one(ProgressBar).advance(20)
