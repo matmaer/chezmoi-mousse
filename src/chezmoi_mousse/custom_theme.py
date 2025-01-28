60685924815b3e32cf44f4730225c1d3fd73a1d3
mm <mm@sofa.local>
2025-01-28 03:12:58 +0100

    Move all theming to new file "custom_theme.py" new file:   src/chezmoi_mousse/custom_theme.py modified:   src/chezmoi_mousse/tui.py modified:   src/chezmoi_mousse/tui.tcss

---
+++ src/chezmoi_mousse/custom_theme.py
@@ -0,0 +1,100 @@
+from textual.theme import Theme
+
+oled_deep_zen = Theme(
+    name="oled-deep-zen", dark=True,
+
+    foreground="#e0e4eb",
+    background="#0d1117",
+    primary="#439cfb",
+    accent="#c392ff",
+    secondary="#24837b",
+    warning="#ad8301",
+    error="#af3029",
+    success="#66800b",
+
+    # old vars to integrate and for reference:
+    # $ui_accent: rgb(98, 118, 147);
+    # $ui_react_border: rgb(12, 114, 22);
+    # $ui_text: rgb(224, 228, 235);
+    # $ui_text: #e0e4eb;
+    # $ui_react_text: rgb(47, 166, 59);
+    # $background: rgb(13, 17, 23);
+    # $ui_dark_accent: rgb(79, 95, 120);
+    # $scrollbar_background: rgb(0, 4, 17);
+    # $fade_start: rgb(67, 156, 251);
+    # $fade_end: rgb(241, 135, 251);
+
+    # vars to check:
+    # "accent-muted"
+    # "block-cursor-background"
+    # "block-cursor-blurred-background"
+    # "block-cursor-blurred-foreground"
+    # "block-cursor-blurred-text-style"
+    # "block-cursor-foreground"
+    # "block-cursor-text-style"
+    # "block-hover-background"
+    # "border-blurred"
+    # "border"
+    # "button-color-foreground"
+    # "button-focus-text-style"
+    # "button-foreground"
+    # "error-muted"
+    # "footer-background"
+    # "footer-description-background"
+    # "footer-description-foreground"
+    # "footer-foreground"
+    # "footer-item-background"
+    # "footer-key-background"
+    # "footer-key-foreground"
+    # "foreground-disabled"
+    # "foreground-muted"
+    # "input-cursor-background"
+    # "input-cursor-foreground"
+    # "input-cursor-text-style"
+    # "input-selection-background"
+    # "link-background-hover"
+    # "link-background"
+    # "link-color-hover"
+    # "link-color"
+    # "link-style-hover"
+    # "link-style"
+    # "primary-muted"
+    # "scrollbar-active"
+    # "scrollbar-background-active"
+    # "scrollbar-background-hover"
+    # "scrollbar-background"
+    # "scrollbar-corner-color"
+    # "scrollbar-hover"
+    # "scrollbar"
+    # "secondary-muted"
+    # "success-muted"
+    # "surface-active"
+    # "text-accent"
+    # "text-disabled"
+    # "text-error"
+    # "text-muted"
+    # "text-primary"
+    # "text-secondary"
+    # "text-success"
+    # "text-warning"
+    # "text"
+    # "warning-muted"
+    # "markdown-h1-color"
+    # "markdown-h1-background"
+    # "markdown-h1-text-style"
+    # "markdown-h2-color"
+    # "markdown-h2-background"
+    # "markdown-h2-text-style"
+    # "markdown-h3-color"
+    # "markdown-h3-background"
+    # "markdown-h3-text-style"
+    # "markdown-h4-color"
+    # "markdown-h4-background"
+    # "markdown-h4-text-style"
+    # "markdown-h5-color"
+    # "markdown-h5-background"
+    # "markdown-h5-text-style"
+    # "markdown-h6-color"
+    # "markdown-h6-background"
+    # "markdown-h6-text-style"
+)
