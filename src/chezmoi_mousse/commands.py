57c3f05965adb5959a6adf756f255b940f58540b
mm <mm@sofa.local>
2025-02-02 16:09:11 +0100

    deleted:    src/chezmoi_mousse/io.py modified:   src/chezmoi_mousse/commands.py

--- src/chezmoi_mousse/commands.py
+++ src/chezmoi_mousse/commands.py
@@ -3,6 +3,28 @@
 import json
 import subprocess
 import tomllib
+from dataclasses import dataclass, field
+
+
+@dataclass
+class ChezmoiIO:
+    input: str = field(default_factory=str)
+    stdout: str = field(default_factory=str)
+    stderr: str = field(default_factory=str)
+
+
+@dataclass
+class ChezmoiOutput(ChezmoiIO):
+    data: ChezmoiIO = field(default_factory=ChezmoiIO)
+    dump_config: ChezmoiIO = field(default_factory=ChezmoiIO)
+    cat_config: ChezmoiIO = field(default_factory=ChezmoiIO)
+    doctor: ChezmoiIO = field(default_factory=ChezmoiIO)
+    ignored: ChezmoiIO = field(default_factory=ChezmoiIO)
+    managed: ChezmoiIO = field(default_factory=ChezmoiIO)
+    status: ChezmoiIO = field(default_factory=ChezmoiIO)
+
+
+chezmoi_io = ChezmoiOutput()
 
 
 class ChezmoiCommands:
