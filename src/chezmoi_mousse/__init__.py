4e1b1dbf8cab3bbb76d821d706d9b41dc4667265
mm <mm@sofa.local>
2025-02-03 17:54:23 +0100

    modified:   src/chezmoi_mousse/__init__.py

--- src/chezmoi_mousse/__init__.py
+++ src/chezmoi_mousse/__init__.py
@@ -1,11 +1,39 @@
 """Singleton to store output for each command in the ChezmoiCommands class"""
 
 CHEZMOI = {
-    "data": dict(),
-    "dump_config": dict(),
-    "cat_config": dict(),
-    "doctor": dict(),
-    "ignored": dict(),
-    "managed": dict(),
-    "unmanaged": dict(),
+    "data": {
+        "command": str(),
+        "result": str(),
+
+    },
+    "dump_config": {
+        "command": str(),
+        "result": str(),
+
+    },
+    "cat_config": {
+        "command": str(),
+        "result": str(),
+
+    },
+    "doctor": {
+        "command": str(),
+        "result": str(),
+
+    },
+    "ignored": {
+        "command": str(),
+        "result": str(),
+
+    },
+    "managed": {
+        "command": str(),
+        "result": str(),
+
+    },
+    "unmanaged": {
+        "command": str(),
+        "result": str(),
+
+    },
 }
