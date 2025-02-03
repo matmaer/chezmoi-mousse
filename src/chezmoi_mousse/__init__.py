ec12e348fb38862d36a2574cda9d364db9c68e2f
mm <mm@sofa.local>
2025-02-03 05:02:23 +0100

    adding singleton src/chezmoi_mousse/__init__.py

--- src/chezmoi_mousse/__init__.py
+++ src/chezmoi_mousse/__init__.py
@@ -1,1 +1,11 @@
-"""The root chezmoi-mousse module."""
+"""Singleton to store output for each command in the ChezmoiCommands class"""
+
+CHEZMOI = {
+    "data": dict(),
+    "dump_config": dict(),
+    "cat_config": dict(),
+    "doctor": dict(),
+    "ignored": dict(),
+    "managed": dict(),
+    "unmanaged": dict(),
+}
