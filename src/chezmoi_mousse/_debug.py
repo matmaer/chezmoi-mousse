069fd806b4999bee0d5514762ea1838bbfc46e6a
mm <mm@sofa.local>
2025-02-06 05:04:55 +0100

    avoid unused code that still can impact objects modified:   src/chezmoi_mousse/_debug.py

--- src/chezmoi_mousse/_debug.py
+++ src/chezmoi_mousse/_debug.py
@@ -1,9 +1,9 @@
-import os
+# import os
 
-from textual.app import ComposeResult
-from textual.containers import ScrollableContainer
-from textual.screen import ModalScreen
-from textual.widgets import Label, Pretty
+# from textual.app import ComposeResult
+# from textual.containers import ScrollableContainer
+# from textual.screen import ModalScreen
+# from textual.widgets import Label, Pretty
 
 # `textual console`: the -x flag can be used to exclude one or more groups:
 
@@ -21,39 +21,39 @@ from textual.widgets import Label, Pretty
 # textual console -x SYSTEM -x EVENT -x DEBUG -x INFO
 
 
-class DebugUtils:
-    @staticmethod
-    def print_env_vars():
-        for key, value in os.environ.items():
-            print(f"{key}: {value}")
+# class DebugUtils:
+#     @staticmethod
+#     def print_env_vars():
+#         for key, value in os.environ.items():
+#             print(f"{key}: {value}")
 
 
-class VariablesScreen(ModalScreen):
-    DEFAULT_CSS = """
-    VariablesScreen {
-        #vars {
-            border: heavy $accent;
-            margin: 2 4;
-            scrollbar-gutter: stable;
-            Static {
-                width: auto;
-            }
-        }
-    }
-    """
-    BINDINGS = [("escape", "dismiss", "Dismiss")]
+# class VariablesScreen(ModalScreen):
+#     DEFAULT_CSS = """
+#     VariablesScreen {
+#         #vars {
+#             border: heavy $accent;
+#             margin: 2 4;
+#             scrollbar-gutter: stable;
+#             Static {
+#                 width: auto;
+#             }
+#         }
+#     }
+#     """
+#     BINDINGS = [("escape", "dismiss", "Dismiss")]
 
-    def __init__(self, local_vars: dict, global_vars: dict) -> None:
-        super().__init__()
-        self.local_vars = local_vars
-        self.global_vars = global_vars
+#     def __init__(self, local_vars: dict, global_vars: dict) -> None:
+#         super().__init__()
+#         self.local_vars = local_vars
+#         self.global_vars = global_vars
 
-    def compose(self) -> ComposeResult:
-        with ScrollableContainer(id="vars"):
-            yield Label("Global Variables", variant="primary")
-            yield Pretty(self.global_vars)
-            yield Label("Local Variables", variant="primary")
-            yield Pretty(self.local_vars)
+#     def compose(self) -> ComposeResult:
+#         with ScrollableContainer(id="vars"):
+#             yield Label("Global Variables", variant="primary")
+#             yield Pretty(self.global_vars)
+#             yield Label("Local Variables", variant="primary")
+#             yield Pretty(self.local_vars)
 
 
 # logging message, example from Jazzhands:
