3a207f213b8c0d0ce0a431c9a4e06823b19c66c1
mm <mm@sofa.local>
2025-02-11 15:20:59 +0100

    moving to commands.py

--- src/chezmoi_mousse/__init__.py
+++ src/chezmoi_mousse/__init__.py
@@ -1,43 +1,12 @@
-"""Singleton to store output for each command"""
-
-from dataclasses import dataclass, field
-
-
-@dataclass
-class ChezmoiOutput:
-    command: str
-    full_command: str = ""
-    output: str = ""
-
-
-@dataclass
-class VerbOutput:
-    data: ChezmoiOutput = field(
-        default_factory=lambda: ChezmoiOutput("data --format=json")
-    )
-    dump_config: ChezmoiOutput = field(
-        default_factory=lambda: ChezmoiOutput("dump-config --format=json")
-    )
-    cat_config: ChezmoiOutput = field(
-        default_factory=lambda: ChezmoiOutput("cat-config")
-    )
-    doctor: ChezmoiOutput = field(
-        default_factory=lambda: ChezmoiOutput("doctor")
-    )
-    ignored: ChezmoiOutput = field(
-        default_factory=lambda: ChezmoiOutput("ignored")
-    )
-    managed: ChezmoiOutput = field(
-        default_factory=lambda: ChezmoiOutput("managed --path-style=absolute")
-    )
-    unmanaged: ChezmoiOutput = field(
-        default_factory=lambda: ChezmoiOutput(
-            "unmanaged --path-style=absolute"
-        )
-    )
-    status: ChezmoiOutput = field(
-        default_factory=lambda: ChezmoiOutput("status --parent-dirs")
-    )
-
-
-CHEZMOI = VerbOutput()
+SPLASH = """\
+ _______ _______ _______ _______ ____ ____ _______ _o_
+|       |   |   |    ___|___    |    ˇ    |       |   |
+|    ===|       |     __'     __|         |   |   |   |
+|       |   |   |       |       |   |ˇ|   |       |   |
+`-------^---^---^-------^-------^---' '---^-------^---'
+   ____ ____ _______ ___ ___ _______ _______ _______
+  |    ˇ    |       |   |   |    ___|    ___|    ___|
+  |         |   |   |   |   |__     |__     |     __|
+  |   |ˇ|   |       |       |       |       |       |
+  '---' '---^-------^-------^-------^-------^-------'
+"""
