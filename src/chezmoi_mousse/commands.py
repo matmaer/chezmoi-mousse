fea587387b3e43a00147f9b744848690199c6a64
mm <mm@sofa.local>
2025-02-13 10:45:14 +0100

    modified:   src/chezmoi_mousse/commands.py

--- src/chezmoi_mousse/commands.py
+++ src/chezmoi_mousse/commands.py
@@ -1,85 +1,103 @@
-"""Module to run shell commands with subprocess."""
+"""
+Module to run shell commands with subprocess.
 
-from collections.abc import Sequence
-from dataclasses import dataclass, field
+CMD_OUTPUT is primitive "cache" to store the last output from subprocess.run().
+The output gets overwritten each time a new call is made when specified.
+For the wrapper, "refresh=False" by default to return this CMD_OUTPUT value.
+"""
+
+# from dataclasses import dataclass
 import shutil
 import subprocess
 
-
-@dataclass
-class FullCommand:
-    command: str = field(default="exit")
-    cmd_args: list = field(default_factory=list)
-    verb_args: list = field(default_factory=list)
-
-    def get_full_cmd_list(self) -> tuple:
-        cmd_list = [self.command] + self.cmd_args + self.verb_args
-        return tuple(cmd_list)
-
-    def get_short_cmd(self) -> list:
-        return [self.command]
+CMD_OUTPUT = {}
 
 
-@dataclass
-class Chezmoi(FullCommand):
-
-    command = shutil.which("chezmoi")
-    cmd_args = [
-        "--no-pager",
-        "--color=false",
-        "--no-tty",
-        "--progress=false",
-    ]
-    verb_dict = {
-        "doctor": ["doctor"],
-        "dump_config": ["dump-config", "--format=json"],
-        "data": ["data", "--format=json"],
-        "cat_config": ["cat-config"],
-        "ignored": ["ignored"],
-        "managed": ["managed"],
-        "status": ["status", "--parent-dirs"],
-        "unmanaged": ["unmanaged", "--path-style=absolute"],
-    }
-
-
-# same logic as the Chezmoi class but with different command and verbs
-@dataclass
-class Git(FullCommand):
-    command = shutil.which("git")
-    cmd_args = [
-        "--no-advice",
-        "--no-pager",
-    ]
-    verb_dict = {
-        "status": [],
-        "log": ["--oneline"],
-    }
-
-
-###########################################################
-# run returns from the stored data or runs subprocess.run #
-###########################################################
-
-
-class ChezmoiIO(FullCommand):
-
-    def __init__(self, refresh: bool = False) -> None:
-        super().__init__()
-        self.short_cmd = self.get_short_cmd()
-        self.full_cmd = self.get_full_cmd_list()
-        self.refresh = refresh
-
-    def get_cmd_output(self):
-        if self.refresh:
-            pass
-
-    def _sub_run(self, cmd_sequence: Sequence) -> subprocess.CompletedProcess:
-        return subprocess.run(
-            cmd_sequence,
+def _get_output(
+    cmd_and_args: list, verb_and_args: str, refresh: bool = False
+) -> str:
+    io_key = f"{cmd_and_args[0]}_{verb_and_args[0]}"
+    full_command = cmd_and_args + verb_and_args
+    refresh = refresh or io_key not in CMD_OUTPUT
+    if refresh:
+        result = subprocess.run(
+            full_command,
             capture_output=True,
             check=True,  # raises exception for any non-zero return code
             shell=False,  # mitigates shell injection risk
             text=True,  # returns stdout as str instead of bytes
             timeout=2,
         )
+        CMD_OUTPUT[io_key] = result.stdout
+    return CMD_OUTPUT[io_key]
 
+
+class Command:
+
+    command: str | None = None
+    #     global_flags: list | None = None
+    #     verbs_with_flags: dict | None = None
+
+    @property
+    def cmd_path(self) -> str:
+        cmd_path = shutil.which(self.command)
+        if not cmd_path:
+            raise FileNotFoundError(
+                f"Command {self.command} not found in PATH"
+            )
+        return cmd_path
+
+
+#     @property
+#     def cmd_and_args(self) -> list:
+#         return [self.command] + self.global_flags
+
+#     @property
+#     def available_verbs(self) -> list:
+#         return self.verbs_with_flags.keys()
+
+
+class Chezmoi(Command):
+
+    command = "chezmoi"
+
+    def __init__(self) -> None:
+        # self.verb = verb
+        self.cmd_with_args = [
+            self.cmd_path,
+            "--no-pager",
+            "--color=false",
+            "--no-tty",
+            "--progress=false",
+        ]
+        self.verbs_with_flags = {
+            "doctor": ["doctor"],
+            "dump_config": ["dump-config", "--format=json"],
+            "data": ["data", "--format=json"],
+            "cat_config": ["cat-config"],
+            "ignored": ["ignored"],
+            "managed": ["managed"],
+            "status": ["status", "--parent-dirs"],
+            "unmanaged": ["unmanaged", "--path-style=absolute"],
+        }
+
+    def run(self, verb) -> str:
+        return _get_output(self.cmd_with_args, self.verbs_with_flags[verb])
+
+
+# @dataclass
+# class Git(Command):
+
+#     command: str = "git"
+#     global_flags: list = [
+#         "--no-advice",
+#         "--no-pager",
+#     ]
+#     verbs_with_flags: dict = {
+#         "status": ["status"],
+#         "log": ["log", "--oneline"],
+#     }
+
+#     @classmethod
+#     def run(cls, verb) -> str:
+#         return _get_output(cls.cmd_and_args, cls.verbs_with_flags[verb])
