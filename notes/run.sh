bf0d3d22f234db99e86ff307397f9d98ae6e24d4
mm <mm@sofa.local>
2025-02-16 21:46:07 +0100

    adding notes

--- notes/run.sh
+++ notes/run.sh
@@ -7,3 +7,13 @@
 # the dir from where the TUI app was started.
 # To check how the shell is customized when it comes to environment variables
 # when chezmoi cd is run.
+
+
+# env var CHEZMOI_SUBSHELL=1 can be used
+
+# also add app exit when shutil cannot find the chezmoi command, no exception is raised by shutil.which
+
+# cmd_path = shutil.which("chezmoi")
+# if cmd_path is None:
+# raise FileNotFoundError(f"Command '{cmd}' not found in PATH")
+
