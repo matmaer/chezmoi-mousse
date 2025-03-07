aeb50fd0c059beed93b07ffd1b4082dfb04e8fde
mm <mm@sofa.local>
2025-03-07 02:16:24 +0100

    move blocks

--- src/chezmoi_mousse/common.py
+++ src/chezmoi_mousse/common.py
@@ -6,112 +6,6 @@ import tomllib
 # from textual.reactive import reactive
 from textual.theme import Theme
 
-SPLASH_7BIT = """\
- _______ _______ _______ _______ ____ ____ _______ _o_
-|       |   |   |    ___|___    |    `    |       |   |
-|    ---|       |     __|     __|         |   |   |   |
-|       |   |   |       |       |   |`|   |       |   |
-`-------^---^---^-------^-------^---' '---^-------^---'
-   ____ ____ _______ ___ ___ _______ _______ _______
-  |    `    |       |   |   |    ___|    ___|    ___|
-  |         |   |   |   |   |__     |__     |     __|
-  |   |`|   |       |       |       |       |       |
-  '---' '---^-------^-------^-------^-------^-------'
-""".splitlines()
-
-SPLASH_8BIT = """\
- _______ _______ _______ _______ ____ ____ _______ _o_
-|       |   |   |    ___|___    |    ˇ    |       |   |
-|    ---|       |     __|     __|         |   |   |   |
-|       |   |   |       |       |   |ˇ|   |       |   |
-`-------^---^---^-------^-------^---' '---^-------^---'
-   ____ ____ _______ ___ ___ _______ _______ _______
-  |    ˇ    |       |   |   |    ___|    ___|    ___|
-  |         |   |   |   |   |__     |__     |     __|
-  |   |ˇ|   |       |       |       |       |       |
-  '---' '---^-------^-------^-------^-------^-------'
-""".splitlines()
-
-SPLASH_ASCII_ART = """\
- _______________________________ _________________ _o_
-|       |   |   |    ___|___    |    '    |       |   |
-|    ===|       |     __|     __|         |   |   |   |
-|       |   |   |       |       |   |ˇ|   |       |   |
-`-------^---^---^-------^-------^---' '---^-------^---'
-   _________________________________________________
-  |    '    |       |   |   |    ___|    ___|    ___|
-  |         |   |   |   |   |__     |__     |     __|
-  |   |ˇ|   |       |       |       |       |       |
-  '---' '---^-------^-------^-------^-------^-------'
-""".replace(
-    "===", "=\u200b=\u200b="
-).splitlines()
-
-SPLASH = SPLASH_ASCII_ART
-
-# # provisional diagrams until dynamically created
-FLOW = """\
-┌──────────────┐    ┌──────────────┐    ┌──────────────┐    ┌──────────────┐
-│home directory│    │ working copy │    │  local repo  │    │ remote repo  │
-└──────┬───────┘    └──────┬───────┘    └──────┬───────┘    └──────┬───────┘
-       │                   │                   │                   │
-       │    chezmoi add    │                   │                   │
-       │   chezmoi re-add  │                   │                   │
-       │──────────────────>│                   │                   │
-       │                   │                   │                   │
-       │   chezmoi apply   │                   │                   │
-       │<──────────────────│                   │                   │
-       │                   │                   │                   │
-       │  chezmoi status   │                   │                   │
-       │   chezmoi diff    │                   │                   │
-       │<─ ─ ─ ─ ─ ─ ─ ─ ─>│                   │     git push      │
-       │                   │                   │──────────────────>│
-       │                   │                   │                   │
-       │                   │           chezmoi git pull            │
-       │                   │<──────────────────────────────────────│
-       │                   │                   │                   │
-       │                   │    git commit     │                   │
-       │                   │──────────────────>│                   │
-       │                   │                   │                   │
-       │                   │    autoCommit     │                   │
-       │                   │──────────────────>│                   │
-       │                   │                   │                   │
-       │                   │                autoPush               │
-       │                   │──────────────────────────────────────>│
-       │                   │                   │                   │
-       │                   │                   │                   │
-┌──────┴───────┐    ┌──────┴───────┐    ┌──────┴───────┐    ┌──────┴───────┐
-│ destination  │    │   staging    │    │   git repo   │    │  git remote  │
-└──────────────┘    └──────────────┘    └──────────────┘    └──────────────┘
-"""
-
-BACKGROUND = "rgb(12, 14, 18)"
-
-oled_dark_zen = Theme(
-    name="oled-dark-zen",
-    dark=True,
-    luminosity_spread=0.9,
-    text_alpha=0.9,
-    accent="rgb(241, 135, 251)",
-    background=BACKGROUND,
-    error="rgb(203, 68, 31)",
-    foreground="rgb(234, 232, 227)",
-    panel="rgb(98, 118, 147)",
-    primary="rgb(67, 156, 251)",
-    secondary="rgb(37, 146, 137)",
-    success="rgb(63, 170, 77)",
-    surface="rgb(24, 28, 34)",
-    warning="rgb(224, 195, 30)",
-    variables={
-        "footer-background": BACKGROUND,
-        "footer-description-background": BACKGROUND,
-        "footer-item-background": BACKGROUND,
-        "footer-key-background": BACKGROUND,
-        "link-background": BACKGROUND,
-        "scrollbar-corner-color": BACKGROUND,
-    },
-)
-
 
 def _subprocess_run(long_command: list[str] | None = None) -> str:
     result = subprocess.run(
@@ -227,5 +121,112 @@ class Chezmoi:
             # map arg_id to the long_command, for looping in LoadingScreen
             self.long_commands[arg_id] = long_cmd
 
-
+# there must be a better way to do this
 chezmoi = Chezmoi()
+
+
+SPLASH_7BIT = """\
+ _______ _______ _______ _______ ____ ____ _______ _o_
+|       |   |   |    ___|___    |    `    |       |   |
+|    ---|       |     __|     __|         |   |   |   |
+|       |   |   |       |       |   |`|   |       |   |
+`-------^---^---^-------^-------^---' '---^-------^---'
+   ____ ____ _______ ___ ___ _______ _______ _______
+  |    `    |       |   |   |    ___|    ___|    ___|
+  |         |   |   |   |   |__     |__     |     __|
+  |   |`|   |       |       |       |       |       |
+  '---' '---^-------^-------^-------^-------^-------'
+""".splitlines()
+
+SPLASH_8BIT = """\
+ _______ _______ _______ _______ ____ ____ _______ _o_
+|       |   |   |    ___|___    |    ˇ    |       |   |
+|    ---|       |     __|     __|         |   |   |   |
+|       |   |   |       |       |   |ˇ|   |       |   |
+`-------^---^---^-------^-------^---' '---^-------^---'
+   ____ ____ _______ ___ ___ _______ _______ _______
+  |    ˇ    |       |   |   |    ___|    ___|    ___|
+  |         |   |   |   |   |__     |__     |     __|
+  |   |ˇ|   |       |       |       |       |       |
+  '---' '---^-------^-------^-------^-------^-------'
+""".splitlines()
+
+SPLASH_ASCII_ART = """\
+ _______________________________ _________________ _o_
+|       |   |   |    ___|___    |    '    |       |   |
+|    ===|       |     __|     __|         |   |   |   |
+|       |   |   |       |       |   |ˇ|   |       |   |
+`-------^---^---^-------^-------^---' '---^-------^---'
+   _________________________________________________
+  |    '    |       |   |   |    ___|    ___|    ___|
+  |         |   |   |   |   |__     |__     |     __|
+  |   |ˇ|   |       |       |       |       |       |
+  '---' '---^-------^-------^-------^-------^-------'
+""".replace(
+    "===", "=\u200b=\u200b="
+).splitlines()
+
+SPLASH = SPLASH_ASCII_ART
+
+# # provisional diagrams until dynamically created
+FLOW = """\
+┌──────────────┐    ┌──────────────┐    ┌──────────────┐    ┌──────────────┐
+│home directory│    │ working copy │    │  local repo  │    │ remote repo  │
+└──────┬───────┘    └──────┬───────┘    └──────┬───────┘    └──────┬───────┘
+       │                   │                   │                   │
+       │    chezmoi add    │                   │                   │
+       │   chezmoi re-add  │                   │                   │
+       │──────────────────>│                   │                   │
+       │                   │                   │                   │
+       │   chezmoi apply   │                   │                   │
+       │<──────────────────│                   │                   │
+       │                   │                   │                   │
+       │  chezmoi status   │                   │                   │
+       │   chezmoi diff    │                   │                   │
+       │<─ ─ ─ ─ ─ ─ ─ ─ ─>│                   │     git push      │
+       │                   │                   │──────────────────>│
+       │                   │                   │                   │
+       │                   │           chezmoi git pull            │
+       │                   │<──────────────────────────────────────│
+       │                   │                   │                   │
+       │                   │    git commit     │                   │
+       │                   │──────────────────>│                   │
+       │                   │                   │                   │
+       │                   │    autoCommit     │                   │
+       │                   │──────────────────>│                   │
+       │                   │                   │                   │
+       │                   │                autoPush               │
+       │                   │──────────────────────────────────────>│
+       │                   │                   │                   │
+       │                   │                   │                   │
+┌──────┴───────┐    ┌──────┴───────┐    ┌──────┴───────┐    ┌──────┴───────┐
+│ destination  │    │   staging    │    │   git repo   │    │  git remote  │
+└──────────────┘    └──────────────┘    └──────────────┘    └──────────────┘
+"""
+
+BACKGROUND = "rgb(12, 14, 18)"
+
+oled_dark_zen = Theme(
+    name="oled-dark-zen",
+    dark=True,
+    luminosity_spread=0.9,
+    text_alpha=0.9,
+    accent="rgb(241, 135, 251)",
+    background=BACKGROUND,
+    error="rgb(203, 68, 31)",
+    foreground="rgb(234, 232, 227)",
+    panel="rgb(98, 118, 147)",
+    primary="rgb(67, 156, 251)",
+    secondary="rgb(37, 146, 137)",
+    success="rgb(63, 170, 77)",
+    surface="rgb(24, 28, 34)",
+    warning="rgb(224, 195, 30)",
+    variables={
+        "footer-background": BACKGROUND,
+        "footer-description-background": BACKGROUND,
+        "footer-item-background": BACKGROUND,
+        "footer-key-background": BACKGROUND,
+        "link-background": BACKGROUND,
+        "scrollbar-corner-color": BACKGROUND,
+    },
+)
