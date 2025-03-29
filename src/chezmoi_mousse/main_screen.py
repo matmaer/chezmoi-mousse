e0514b75cfc6ac4fa6e6924fc0eb501511448958
mm <mm@sofa.local>
2025-03-29 18:28:10 +0100

    add view source

--- src/chezmoi_mousse/main_screen.py
+++ src/chezmoi_mousse/main_screen.py
@@ -1,19 +1,23 @@
-from pathlib import Path
+import inspect
 from collections.abc import Iterable
+from pathlib import Path
 
+from rich.syntax import Syntax
 from rich.text import Text
+from textual import work
 from textual.app import ComposeResult
-from textual.containers import VerticalScroll
+from textual.binding import Binding
+from textual.containers import ScrollableContainer, VerticalScroll
 from textual.reactive import reactive
-from textual.screen import Screen
+from textual.screen import ModalScreen, Screen
 
 # from textual.types import DirEntry
 from textual.widget import Widget
 from textual.widgets import (
+    Checkbox,
     Collapsible,
     DataTable,
     DirectoryTree,
-    Checkbox,
     Footer,
     Header,
     Link,
@@ -26,6 +30,106 @@ from textual.widgets import (
 )
 
 from chezmoi_mousse.common import FLOW, chezmoi
+
+
+class CodeScreen(ModalScreen):
+    DEFAULT_CSS = """
+    CodeScreen {
+        #code {
+            border: heavy $accent;
+            margin: 2 4;
+            scrollbar-gutter: stable;
+            Static {
+                width: auto;
+            }
+        }
+    }
+    """
+    BINDINGS = [("escape", "dismiss", "Dismiss code")]
+
+    def __init__(self, title: str, code: str) -> None:
+        super().__init__()
+        self.code = code
+        self.title = title
+
+    def compose(self) -> ComposeResult:
+        with ScrollableContainer(id="code"):
+            yield Static(
+                Syntax(
+                    self.code,
+                    lexer="python",
+                    indent_guides=True,
+                    line_numbers=True,
+                ),
+                expand=True,
+            )
+
+    def on_mount(self):
+        code_widget = self.query_one("#code")
+        code_widget.border_title = self.title
+        code_widget.border_subtitle = "Escape to close"
+
+
+class PageScreen(Screen):
+    DEFAULT_CSS = """
+    PageScreen {
+        width: 100%;
+        height: 1fr;
+        overflow-y: auto;
+    }
+    """
+    BINDINGS = [
+        Binding(
+            "c",
+            "show_code",
+            "Code",
+            tooltip="Show the code used to generate this screen",
+        )
+    ]
+
+    @work(thread=True)
+    def get_code(self, source_file: str) -> str | None:
+        """Read code from disk, or return `None` on error."""
+        try:
+            with open(source_file, "rt", encoding="utf-8") as file_:
+                return file_.read()
+        except Exception:
+            return None
+
+    async def action_show_code(self):
+        source_file = inspect.getsourcefile(self.__class__)
+        if source_file is None:
+            self.notify(
+                "Could not get the code for this page",
+                title="Show code",
+                severity="error",
+            )
+            return
+
+        code = await self.get_code(source_file).wait()
+        if code is None:
+            self.notify(
+                "Could not get the code for this page",
+                title="Show code",
+                severity="error",
+            )
+        else:
+            self.app.push_screen(CodeScreen("Code for this page", code))
+
+
+# class Operate(ModalScreen):
+
+#     def compose(self) -> ComposeResult:
+#         yield Collapsible(
+#             Static("This is a modal screen"),
+#             title="Modal",
+#         )
+
+#     def on_button_pressed(self, event: Button.Pressed) -> None:
+#         if event.button.id == "quit":
+#             self.app.exit()
+#         else:
+#             self.app.pop_screen()
 
 
 class GitLog(DataTable):
@@ -279,6 +383,13 @@ class MousseTree(DirectoryTree):  # pylint: disable=too-many-ancestors
             return paths
         return [p for p in paths if p not in chezmoi.get_managed_paths]
 
+    # def on_directory_tree_file_selected(
+    #     self, event: DirectoryTree.FileSelected
+    # ) -> None:
+    #     """Called when the user click a file in the directory tree."""
+    #     event.stop()
+    #     self.path = str(event.path)
+
     # def _on_tree_node_selected(
     #     self, event: Tree.NodeSelected[DirEntry]
     # ) -> None:
@@ -310,10 +421,11 @@ class ManagedDirTree(Widget):
 
 class MainScreen(Screen):
 
-    BINDINGS = {
-        ("i, I", "toggle_slidebar", "Toggle Inspect"),
-        ("s, S", "toggle_spacing", "Toggle Spacing"),
-    }
+    BINDINGS = [
+        Binding("i, I", "toggle_slidebar", "Toggle Inspect"),
+        Binding("s, S", "toggle_spacing", "Toggle Spacing"),
+        # Binding("escape", "blur", "Unfocus any focused widget", show=False),
+    ]
 
     def compose(self) -> ComposeResult:
 
