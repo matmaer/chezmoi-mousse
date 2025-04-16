89d5dc0941299ef1cf363290bcb1117cc7081736
mm <mm@sofa.local>
2025-04-16 19:38:10 +0200

    profiling branch modified:   src/chezmoi_mousse/__init__.py

--- src/chezmoi_mousse/__init__.py
+++ src/chezmoi_mousse/__init__.py
@@ -1,9 +1,20 @@
+import cProfile
+import pstats
 from chezmoi_mousse.tui import ChezmoiTUI
 
 
 def main():
+    profiler = cProfile.Profile()
+    profiler.enable()
+
     app = ChezmoiTUI()
     app.run(inline=False, headless=False, mouse=True)
+
+    profiler.disable()
+    stats = pstats.Stats(profiler)
+    stats.strip_dirs()
+    stats.sort_stats("cumulative")
+    stats.print_stats(20)  # Print the top 20 functions by cumulative time
 
 
 if __name__ == "__main__":
