2e1893a6555e8f4d5c79f478fc8efcefb0d93253
mm <mm@sofa.local>
2025-04-19 17:30:10 +0200

    restructure project

--- src/chezmoi_mousse/mousse.py
+++ src/chezmoi_mousse/mousse.py
@@ -1,3 +1,5 @@
+"""Contains the widgets used to compose the main screen of chezmoi-mousse."""
+
 from collections.abc import Iterable
 from pathlib import Path
 
@@ -11,7 +13,7 @@ from textual.containers import (
 )
 from textual.content import Content
 from textual.reactive import reactive
-from textual.screen import ModalScreen, Screen
+from textual.screen import ModalScreen
 from textual.widget import Widget
 from textual.widgets import (
     Button,
@@ -26,6 +28,122 @@ from textual.widgets import (
 import chezmoi_mousse.factory as factory
 from chezmoi_mousse import FLOW
 from chezmoi_mousse.chezmoi import chezmoi
+
+
+from rich.text import Text
+from textual.app import ComposeResult
+from textual.containers import VerticalScroll
+from textual.widget import Widget
+from textual.widgets import (
+    Collapsible,
+    DataTable,
+    Label,
+    Link,
+    ListItem,
+    ListView,
+    Pretty,
+    Static,
+)
+
+from chezmoi_mousse.chezmoi import chezmoi
+from chezmoi_mousse.config import pw_mgr_info
+
+
+class GitLog(DataTable):
+
+    def __init__(self) -> None:
+        super().__init__(id="gitlog", cursor_type="row")
+
+    def on_mount(self) -> None:
+        self.add_columns("COMMIT", "MESSAGE")
+        for line in chezmoi.git_log.list_out:
+            columns = line.split(";")
+            self.add_row(*columns)
+
+
+class Doctor(Widget):
+
+    def compose(self) -> ComposeResult:
+        yield DataTable(id="doctortable", show_cursor=False)
+        with VerticalScroll(can_focus=False):
+            yield Collapsible(
+                ListView(id="cmdnotfound"),
+                title="Commands Not Found",
+                classes="collapsible-defaults",
+            )
+            yield Collapsible(
+                Pretty(chezmoi.dump_config.dict_out),
+                title="chezmoi dump-config",
+                classes="collapsible-defaults",
+            )
+            yield Collapsible(
+                Pretty(chezmoi.template_data.dict_out),
+                title="chezmoi data (template data)",
+                classes="collapsible-defaults",
+            )
+            yield Collapsible(
+                GitLog(),
+                title="chezmoi git log (last 20 commits)",
+                classes="collapsible-defaults",
+            )
+            yield Collapsible(
+                Pretty(chezmoi.cat_config.list_out),
+                title="chezmoi cat-config (contents of config-file)",
+                classes="collapsible-defaults",
+            )
+            yield Collapsible(
+                Pretty(chezmoi.ignored.list_out),
+                title="chezmoi ignored (git ignore in source-dir)",
+                classes="collapsible-defaults",
+            )
+
+    def on_mount(self) -> None:
+
+        styles = {
+            "ok": f"{self.app.current_theme.success}",
+            "warning": f"{self.app.current_theme.warning}",
+            "error": f"{self.app.current_theme.error}",
+            "info": f"{self.app.current_theme.foreground}",
+        }
+
+        list_view = self.query_exactly_one("#cmdnotfound", ListView)
+        table = self.query_exactly_one("#doctortable", DataTable)
+        doctor_rows = chezmoi.doctor.list_out
+        table.add_columns(*doctor_rows[0].split())
+
+        for line in doctor_rows[1:]:
+            row = tuple(line.split(maxsplit=2))
+            if row[0] == "info" and "not found in $PATH" in row[2]:
+                if row[1] in pw_mgr_info:
+                    list_view.append(
+                        ListItem(
+                            Link(row[1], url=pw_mgr_info[row[1]]["link"]),
+                            Static(pw_mgr_info[row[1]]["description"]),
+                        )
+                    )
+                elif row[1] not in pw_mgr_info:
+                    list_view.append(
+                        ListItem(
+                            # color accent as that's how links are styled by default
+                            Static(f"[$accent-muted]{row[1]}[/]", markup=True),
+                            Label(
+                                "Not Found in $PATH, no description available in TUI."
+                            ),
+                        )
+                    )
+            elif row[0] == "ok" or row[0] == "warning" or row[0] == "error":
+                row = [
+                    Text(cell_text, style=f"{styles[row[0]]}")
+                    for cell_text in row
+                ]
+            elif row[0] == "info" and row[2] == "not set":
+                row = [
+                    Text(cell_text, style=f"{self.app.current_theme.warning}")
+                    for cell_text in row
+                ]
+            else:
+                row = [Text(cell_text) for cell_text in row]
+            table.add_row(*row)
 
 
 class ChezmoiStatus(VerticalScroll):
