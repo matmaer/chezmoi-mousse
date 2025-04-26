cb20f21b815a706c7d2a48c3cb5a8010d0d0bca7
mm <mm@sofa.local>
2025-04-26 19:59:37 +0200

    add **kwargs to pass an id when multiple SlideBar instances exist

--- src/chezmoi_mousse/components.py
+++ src/chezmoi_mousse/components.py
@@ -308,10 +308,10 @@ class SlideBar(VerticalGroup):
                 tooltip=self.switch_tooltip
             )
 
-    def __init__(self, filters: dict) -> None:
+    def __init__(self, filters: dict, **kwargs) -> None:
         self.filters = filters
         self.filter_items = []
-        super().__init__()
+        super().__init__(**kwargs)
 
     def compose(self) -> ComposeResult:
         yield from self.filter_items
