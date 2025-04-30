b1ae7b377c9e04a5b14649dbc6ddef06db3375da
mm <mm@sofa.local>
2025-05-01 01:58:58 +0200

    demo how the PR would work for the user

--- src/chezmoi_mousse/__main__.py
+++ src/chezmoi_mousse/__main__.py
@@ -1,8 +1,21 @@
+import rich.repr
+from typing import ClassVar
+from textual.scrollbar import ScrollBar, ScrollBarRender
 from chezmoi_mousse.gui import ChezmoiTUI
+
+
+class CustomScrollBarRender(ScrollBarRender):
+    SLIM_HORIZONTAL_BAR: ClassVar[str] | None = "▂"
+
+
+@rich.repr.auto
+class CustomScrollBar(ScrollBar):
+    renderer: ClassVar[type[ScrollBarRender]] = CustomScrollBarRender
 
 
 def main():
     app = ChezmoiTUI()
+    ScrollBar.renderer = CustomScrollBarRender
     app.run(inline=False, headless=False, mouse=True)
 
 
