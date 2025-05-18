4b85640c1de973181a6e17ee6668a21892e7d598
mm <mm@sofa.local>
2025-05-18 21:13:18 +0200

    init theme outside of on_mount method

--- src/chezmoi_mousse/gui.py
+++ src/chezmoi_mousse/gui.py
@@ -27,6 +27,21 @@ class MainScreen(Screen):
         yield Footer()
 
 
+chezmoi_mousse_dark = Theme(
+    name="chezmoi-mousse-dark",
+    dark=True,
+    accent="#F187FB",
+    background="#000000",
+    error="#ba3c5b",  # textual dark
+    foreground="#DEDAE1",
+    primary="#0178D4",  # textual dark
+    secondary="#004578",  # textual dark
+    surface="#101010",  # see also textual/theme.py
+    success="#4EBF71",  # textual dark
+    warning="#ffa62b",  # textual dark
+)
+
+
 class ChezmoiTUI(App):
 
     CSS_PATH = "gui.tcss"
@@ -35,21 +50,7 @@ class ChezmoiTUI(App):
 
     def on_mount(self) -> None:
         self.title = "-  c h e z m o i  m o u s s e  -"
-        self.register_theme(
-            Theme(
-                name="chezmoi-mousse-dark",
-                dark=True,
-                accent="#F187FB",
-                background="#000000",
-                error="#ba3c5b",  # textual dark
-                foreground="#DEDAE1",
-                primary="#0178D4",  # textual dark
-                secondary="#004578",  # textual dark
-                surface="#101010",  # see also textual/theme.py
-                success="#4EBF71",  # textual dark
-                warning="#ffa62b",  # textual dark
-            )
-        )
+        self.register_theme(chezmoi_mousse_dark)
         self.theme = "chezmoi-mousse-dark"
         self.push_screen("loading", self.push_main_screen)
 
