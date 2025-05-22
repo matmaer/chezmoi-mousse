17f5b661a089006867dad4c9e7a8dcc38399b07b
mm <mm@sofa.local>
2025-05-22 08:32:08 +0200

    auto dismiss loading screen and have it rendered from the first few milli second on almost any hardware

--- src/chezmoi_mousse/splash.py
+++ src/chezmoi_mousse/splash.py
@@ -9,59 +9,52 @@ from textual.color import Gradient
 from textual.containers import Center, Middle
 from textual.screen import Screen
 from textual.strip import Strip
-from textual.widgets import Button, RichLog, Static
+from textual.widgets import RichLog, Static
 
 from chezmoi_mousse import SPLASH
 from chezmoi_mousse.chezmoi import chezmoi
 
+start_color = "#0178D4"
+end_color = "#F187FB"
+fade = [start_color] * 5
+gradient = Gradient.from_colors(start_color, end_color, quality=5)
+fade.extend([color.hex for color in gradient.colors])
+gradient.colors.reverse()
+fade.extend([color.hex for color in gradient.colors])
+line_styles = deque([Style(color=color, bold=True) for color in fade])
+fade_height = len(SPLASH)
+fade_width = len(max(SPLASH, key=len))
+
 
 class AnimatedFade(Static):
 
-    def __init__(self, line_styles: deque[Style]) -> None:
-        self.line_styles = line_styles
+    def __init__(self) -> None:
         super().__init__()
-        self.styles.height = len(SPLASH)
-        self.styles.width = len(max(SPLASH, key=len))
+        self.styles.height = fade_height
+        self.styles.width = fade_width
 
     def render_lines(self, crop) -> list[Strip]:
-        self.line_styles.rotate()
+        line_styles.rotate()
         return super().render_lines(crop)
 
     def render_line(self, y: int) -> Strip:
-        return Strip([Segment(SPLASH[y], style=self.line_styles[y])])
+        return Strip([Segment(SPLASH[y], style=line_styles[y])])
 
     def on_mount(self) -> None:
-        self.set_interval(interval=0.11, callback=self.refresh)
+        self.set_interval(interval=0.05, callback=self.refresh)
 
 
 class LoadingScreen(Screen):
 
     def __init__(self) -> None:
-        self.animated_fade = AnimatedFade(line_styles=self.create_fade())
+        self.animated_fade = AnimatedFade()
         self.dest_dir: Path
         super().__init__(id="loading_screen")
-
-    def create_fade(self) -> deque[Style]:
-        start_color = "#0178D4"
-        end_color = "#F187FB"
-        fade = [start_color] * 5
-        gradient = Gradient.from_colors(start_color, end_color, quality=5)
-        fade.extend([color.hex for color in gradient.colors])
-        gradient.colors.reverse()
-        fade.extend([color.hex for color in gradient.colors])
-        return deque([Style(color=color, bold=True) for color in fade])
 
     def compose(self) -> ComposeResult:
         with Middle():
             yield Center(self.animated_fade)
             yield Center(RichLog(id="loading_screen_log"))
-            yield Center(
-                Button(
-                    id="continue_button",
-                    label="press any key or click to continue",
-                    disabled=True,
-                )
-            )
 
     def log_text(self, log_label: str) -> None:
         padding = 32 - len(log_label)
@@ -87,11 +80,8 @@ class LoadingScreen(Screen):
             for worker in self.app.workers
             if worker.group == "io_workers"
         ):
-            self.set_dest_dir()
-            self.query_one("#continue_button", Button).disabled = False
-
-    def set_dest_dir(self) -> None:
-        chezmoi.dest_dir = self.dest_dir
+            chezmoi.dest_dir = self.dest_dir
+            self.dismiss()
 
     def on_mount(self) -> None:
 
@@ -102,12 +92,4 @@ class LoadingScreen(Screen):
         for arg_id in to_process:
             self.run_io_worker(arg_id)
 
-        self.set_interval(interval=0.1, callback=self.all_workers_finished)
-
-    def on_key(self) -> None:
-        if not self.query_one("#continue_button", Button).disabled:
-            self.dismiss()
-
-    def on_click(self) -> None:
-        if not self.query_one("#continue_button", Button).disabled:
-            self.dismiss()
+        self.set_interval(interval=0.4, callback=self.all_workers_finished)
