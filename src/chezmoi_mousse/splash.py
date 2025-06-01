5c8801b518818977173080360625ce3aa995b527
mm <mm@sofa.local>
2025-06-01 11:54:15 +0200

    have chemoi doctor run first

--- src/chezmoi_mousse/splash.py
+++ src/chezmoi_mousse/splash.py
@@ -117,5 +117,10 @@ class LoadingScreen(Screen):
 
     def on_mount(self) -> None:
         self.set_interval(interval=1, callback=self.all_workers_finished)
+        # first run chezzmoi doctor as it is the most expensive command so the
+        # other threads can run while it's being executed
+        self.run_io_worker("doctor")
+        LONG_COMMANDS.pop("doctor")
+        # the doctor command has been removed from LONG_COMMANDS
         for arg_id in LONG_COMMANDS:
             self.run_io_worker(arg_id)
