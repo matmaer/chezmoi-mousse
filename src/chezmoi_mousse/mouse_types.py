978bbea86e87ad17615e5deeda21684ce4af7925
mm <mm@sofa.local>
2025-06-03 00:13:12 +0200

    introduce mouse types

---
+++ src/chezmoi_mousse/mouse_types.py
@@ -0,0 +1,5 @@
+from typing import Literal
+
+TabLabels = Literal["Apply", "ReAdd", "Add", "Doctor", "Diagram", "Log"]
+TabButtons = Literal["Tree", "List", "Content", "Diff", "Git-Log"]
+ButtonAreas = Literal["TopLeft", "TopRight", "BottomRight", "BottomLeft"]
