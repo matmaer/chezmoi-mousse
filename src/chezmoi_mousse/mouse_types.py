dcbc10fe932b7897c45cee1d9e877f43410119a7
mm <mm@sofa.local>
2025-06-04 13:23:42 +0200

    start simple type checking

--- src/chezmoi_mousse/mouse_types.py
+++ src/chezmoi_mousse/mouse_types.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import Literal, TypedDict
+from typing import Literal
 
 
 type TabLabel = Literal["Apply", "Re-Add", "Add"]
@@ -8,49 +8,4 @@ type ButtonArea = Literal["TopLeft", "TopRight", "BottomRight"]
 
 type ButtonLabel = Literal["Tree", "List", "Content", "Diff", "Git-Log"]
 
-
-class TreeSpecDict(TypedDict, total=True):
-    """Dictionary to pass for tree_spec, with required tab_label and tree_kind
-    keys.
-
-    Logic in TreeView depends on these.
-    """
-
-    # key has to be "tab_label", value is an ApplyReAddLabel Literal type alias
-    tab_label: TabLabel
-    # key has to be "tree_kind", value will be either "Tree" or "List"
-    tree_kind: ButtonLabel
-
-
-type TreeSpec = TreeSpecDict
-
-
-class PathViewSpecDict(TypedDict, total=True):
-    """Dictionary to pass for path_spec, with required path and tab_label keys.
-
-    Logic in PathView depends on these.
-    """
-
-    # key has to be "path", value is a Path object
-    path: Path
-    # key has to be "tab_label", value is an ApplyReAddLabel Literal type alias
-    tab_label: TabLabel
-
-
-type PathViewSpec = PathViewSpecDict | None
-
-
-class DiffSpecDict(TypedDict, total=True):
-    """Dictionary to pass for diff_spec, with required path and tab_label keys
-    and value types.
-
-    Logic in DiffView depends on these.
-    """
-
-    # key has to be "path", value is a Path object
-    path: Path
-    # key has to be "tab_label", value is an ApplyReAddLabel Literal type alias
-    tab_label: TabLabel
-
-
-type DiffSpec = DiffSpecDict | None
+type DiffSpec = tuple[Path, str] | None
