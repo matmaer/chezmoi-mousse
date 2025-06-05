b27f25c631e095ad2602743dfeb8aa9d5ca6debf
mm <mm@sofa.local>
2025-06-05 07:02:37 +0200

    apply auto-width

--- src/chezmoi_mousse/main_tabs.py
+++ src/chezmoi_mousse/main_tabs.py
@@ -49,7 +49,7 @@ from chezmoi_mousse.mouse_types import TabLabel, ButtonLabel
 class TabButton(Vertical):
 
     def __init__(self, label: ButtonLabel, button_id: str) -> None:
-        super().__init__()
+        super().__init__(classes="auto-width")
         self.button_id = button_id
         self.label = label
 
@@ -91,9 +91,10 @@ class TreeTabSwitchers(Horizontal):
 
     def compose(self) -> ComposeResult:
         # Left: Tree/List Switcher
-        with Vertical(classes="tab-content-left"):
+        with Vertical(classes="auto-width tab-content-left"):
             with HorizontalGroup(
-                id=self.tree_button_group_id, classes="tab-buttons-horizontal"
+                id=self.tree_button_group_id,
+                classes="auto-width tab-buttons-horizontal",
             ):
                 yield TabButton("Tree", self.tree_button_id)
                 yield TabButton("List", self.list_button_id)
@@ -116,7 +117,7 @@ class TreeTabSwitchers(Horizontal):
                         classes="auto-width",
                     )
             with HorizontalGroup(
-                classes="filter-container filter-border-top filter-border-bottom"
+                classes="auto-width filter-container filter-border-top filter-border-bottom"
             ):
                 yield Switch(id=self.filter_switch_id, classes="filter-switch")
                 yield Label(
@@ -126,7 +127,8 @@ class TreeTabSwitchers(Horizontal):
         # Right: Content/Diff Switcher
         with Vertical():
             with HorizontalGroup(
-                id=self.view_buttons_id, classes="tab-buttons-horizontal"
+                id=self.view_buttons_id,
+                classes="auto-width tab-buttons-horizontal",
             ):
                 yield TabButton("Content", self.content_button_id)
                 yield TabButton("Diff", self.diff_button_id)
@@ -135,7 +137,7 @@ class TreeTabSwitchers(Horizontal):
                 with ContentSwitcher(
                     id=self.view_switcher_id,
                     initial=self.content_content_id,
-                    classes="top-border-title-style",
+                    classes="auto-width, top-border-title-style",
                 ):
                     yield PathView(
                         id=self.content_content_id,
@@ -299,7 +301,9 @@ class AddTab(Horizontal):
 
     def compose(self) -> ComposeResult:
         # left
-        with Vertical(id=self.tree_container_id, classes="tab-content-left"):
+        with Vertical(
+            id=self.tree_container_id, classes="auto-width tab-content-left"
+        ):
             yield ScrollableContainer(
                 FilteredDirTree(chezmoi.dest_dir, id=self.add_tree_id),
                 id=self.tree_container_id,
@@ -311,7 +315,7 @@ class AddTab(Horizontal):
             #     return True
 
             with VerticalGroup(
-                classes="filter-container filter-border-top filter-border-bottom"
+                classes="auto-width filter-container filter-border-top filter-border-bottom"
             ):
                 yield HorizontalGroup(
                     Switch(
