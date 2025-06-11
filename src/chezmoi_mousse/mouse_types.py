899bfb71b585b7a0516caa391b5834ebd4022d6c
mm <mm@sofa.local>
2025-06-11 14:44:49 +0200

    add TabSide type alial and "BottomRight" Literal to ButtonArea

--- src/chezmoi_mousse/mouse_types.py
+++ src/chezmoi_mousse/mouse_types.py
@@ -1,18 +1,16 @@
 from typing import Literal
 
-type ApplyLabel = Literal["Apply"]
-type ReAddLabel = Literal["Re-Add"]
-type AddLabel = Literal["Add"]
-
 # chezmoi status: added, deleted, modified, and no status
 type StatusCode = Literal["A", "D", "M", "X"]
 
-type TabLabel = Literal[ApplyLabel, ReAddLabel, AddLabel]
+type TabLabel = Literal["Apply", "Re-Add", "Add"]
 
 type ButtonLabel = Literal["Tree", "List", "Contents", "Diff", "Git-Log"]
 
-type ButtonArea = Literal["TopLeft", "TopRight"]
+type ButtonArea = Literal["TopLeft", "TopRight", "BottomRight"]
 
 type FilterName = Literal["unchanged", "unwanted", "unmanaged_dirs"]
 
 type TreeName = Literal["ManagedTree", "ManTreeList", "DirTree"]
+
+type TabSide = Literal["Left", "Right"]
