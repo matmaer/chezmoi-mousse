b110d4f311182625a5b01188722b4603206e7f06
mm <mm@sofa.local>
2025-06-11 21:07:14 +0200

    modified:   src/chezmoi_mousse/mouse_types.py

--- src/chezmoi_mousse/mouse_types.py
+++ src/chezmoi_mousse/mouse_types.py
@@ -1,16 +1,12 @@
 from typing import Literal
 
-# chezmoi status: added, deleted, modified, and no status
-type StatusCode = Literal["A", "D", "M", "X"]
-
-type TabLabel = Literal["Apply", "Re-Add", "Add"]
-
-type ButtonLabel = Literal["Tree", "List", "Contents", "Diff", "Git-Log"]
-
+# first define TreeName, as it is used in other type aliases
+type TreeName = Literal["TreeTree", "TreeList", "FilteredDirTree"]
+# sorted alphabetically:
 type ButtonArea = Literal["TopLeft", "TopRight", "BottomRight"]
-
+type ButtonLabel = Literal["Tree", "List", "Contents", "Diff", "Git-Log"]
+type ComponentName = Literal[TreeName, "PathView", "DiffView", "GitLog"]
 type FilterName = Literal["unchanged", "unwanted", "unmanaged_dirs"]
-
-type TreeName = Literal["ManagedTree", "ManTreeList", "DirTree"]
-
+type StatusCode = Literal["A", "D", "M", "X"]
+type TabLabel = Literal["Apply", "Re-Add", "Add"]
 type TabSide = Literal["Left", "Right"]
