47cebad58608548563dfb53a3d039c3dcb777539
matmaer <22435003+matmaer@users.noreply.github.com>
2025-06-13 21:53:50 +0200

    correct wrong re-naming

---
+++ src/chezmoi_mousse/type_aliasses.py
@@ -0,0 +1,24 @@
+from typing import Literal
+
+# Type aliases not to be imported or used directly
+type OperationButtonLabel = Literal[
+    "Add Directory",
+    "Add File",
+    "Apply Directory",
+    "Apply File",
+    "Re-Add Directory",
+    "Re-Add File",
+]
+type TreeName = Literal["AddTree", "ExpandedTree", "FlatTree", "ManagedTree"]
+type TabButtonLabel = Literal["Tree", "List", "Contents", "Diff", "Git-Log"]
+type VerticalAreaName = Literal["Left", "Right"]
+type SquareAreaName = Literal["TopLeft", "TopRight", "BottomRight"]
+
+# Type aliases to be used instead of the above
+type Area = Literal[VerticalAreaName, SquareAreaName]
+type ButtonLabel = Literal[TabButtonLabel, OperationButtonLabel]
+type ComponentName = Literal[TreeName, "PathView", "DiffView", "GitLog"]
+type FilterName = Literal[
+    "expand_all", "unchanged", "unwanted", "unmanaged_dirs"
+]
+type TabLabel = Literal["Apply", "Re-Add", "Add", "Doctor", "Diagram", "Log"]
