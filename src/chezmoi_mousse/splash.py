652d397ac414287cbb7f55c43f231536bcc8f9be
matmaer <22435003+matmaer@users.noreply.github.com>
2025-06-20 07:57:28 +0200

    modified:   src/chezmoi_mousse/splash.py

--- src/chezmoi_mousse/splash.py
+++ src/chezmoi_mousse/splash.py
@@ -1,7 +1,6 @@
 """Initialize all data in the InputOutput dataclasses for chezmoi.py."""
 
 from collections import deque
-from pathlib import Path
 
 from rich.segment import Segment
 from rich.style import Style
@@ -31,18 +30,25 @@ SPLASH = """\
 ).splitlines()
 
 
-start_color = "#0178D4"
-end_color = "#F187FB"
-fade = [start_color] * 7
-gradient = Gradient.from_colors(start_color, end_color, quality=4)
-fade.extend([color.hex for color in gradient.colors])
-gradient.colors.reverse()
-fade.extend([color.hex for color in gradient.colors])
-line_styles = deque([Style(color=color, bold=True) for color in fade])
-fade_height = len(SPLASH)
-fade_width = len(max(SPLASH, key=len)) - 2
+def create_deque() -> deque[Style]:
+    start_color = "#0178D4"
+    end_color = "#F187FB"
 
-DEST_DIR: Path
+    fade = [start_color] * 8
+    gradient = Gradient.from_colors(start_color, end_color, quality=6)
+    fade.extend([color.hex for color in gradient.colors])
+    gradient.colors.reverse()
+    fade.extend([color.hex for color in gradient.colors])
+
+    line_styles = deque([Style(color=color, bold=True) for color in fade])
+    return line_styles
+
+
+LINE_STYLES = create_deque()
+LINE_STYLES.rotate(-2)
+SPLASH_HEIGHT = len(SPLASH)
+SPLASH_WIDTH = len(max(SPLASH, key=len))
+
 LOG_PADDING_WIDTH = 36
 LONG_COMMANDS = chezmoi.long_commands
 
@@ -60,34 +66,31 @@ class AnimatedFade(Static):
 
     def __init__(self) -> None:
         super().__init__()
-        self.styles.height = fade_height
-        self.styles.width = fade_width
-        self.styles.margin = 2
+        self.styles.height = SPLASH_HEIGHT
+        self.styles.width = SPLASH_WIDTH
+        self.styles.margin = 1
 
     def render_lines(self, crop) -> list[Strip]:
-        line_styles.rotate()
+        LINE_STYLES.rotate()
         return super().render_lines(crop)
 
     def render_line(self, y: int) -> Strip:
-        return Strip([Segment(SPLASH[y], style=line_styles[y])])
+        return Strip([Segment(SPLASH[y], style=LINE_STYLES[y])])
 
     def on_mount(self) -> None:
-        self.set_interval(interval=0.1, callback=self.refresh)
-
-
-ANIMATED_FADE = AnimatedFade()
+        self.set_interval(interval=0.05, callback=self.refresh)
 
 
 class LoadingScreen(Screen):
 
     def compose(self) -> ComposeResult:
-        yield Middle(Center(ANIMATED_FADE), Center(RICH_LOG))
+        yield Middle(Center(AnimatedFade()), Center(RICH_LOG))
 
     def log_text(self, log_label: str) -> None:
         padding = LOG_PADDING_WIDTH - len(log_label)
+        log_text = f"{log_label} {'.' * padding} loaded"
 
         def update_log():
-            log_text = f"{log_label} {'.' * padding} loaded"
             RICH_LOG.write(log_text)
 
         self.app.call_from_thread(update_log)
@@ -114,11 +117,9 @@ class LoadingScreen(Screen):
             self.dismiss(COMMAND_LOG)
 
     def on_mount(self) -> None:
-        # first run chezzmoi doctor as it is the most expensive command so the
-        # other threads can run while it's being executed
+        # first run chezzmoi doctor, most expensive command
         self.run_io_worker("doctor")
         LONG_COMMANDS.pop("doctor")
-        # the doctor command has been removed from LONG_COMMANDS
         for arg_id in LONG_COMMANDS:
             self.run_io_worker(arg_id)
-        self.set_interval(interval=1.2, callback=self.all_workers_finished)
+        self.set_interval(interval=1, callback=self.all_workers_finished)
