c720b8c1c3d9ebe1de26d4a53b1a3462c8898a6b
matmaer <22435003+matmaer@users.noreply.github.com>
2025-07-08 06:28:53 +0200

    init this in __init__.py so app doesn't start if this is not possible and allow to restructure the textual App initialization

---
+++ src/chezmoi_mousse/__init__.py
@@ -0,0 +1,35 @@
+import json
+import sys
+from pathlib import Path
+from typing import Any
+
+from chezmoi_mousse.chezmoi import BASE, subprocess_run
+
+chezmoi_config: dict[str, Any]
+
+try:
+    chezmoi_config: dict[str, Any] = json.loads(
+        subprocess_run(BASE + ("dump-config",))
+    )
+except Exception as e:
+    print(f"Failed run chezmoi dump-config: {e}", file=sys.stderr)
+    sys.exit(1)
+
+
+class ChezmoiConfig:
+    def __init__(self, config: dict[str, Any]):
+        self.destDir: Path = Path(config["destDir"])
+        self.source_dir: Path = Path(config["sourceDir"])
+        self.auto_add: bool = config["git"]["autoadd"]
+        self.auto_commit: bool = config["git"]["autocommit"]
+        self.auto_push: bool = config["git"]["autopush"]
+        self.interactive: bool = config["interactive"]
+        self.mode: str = config["mode"]
+        self.tempDir: str = config["tempDir"]
+
+
+try:
+    CM_CFG = ChezmoiConfig(chezmoi_config)
+except Exception as e:
+    print(f"Failed to create ChezmoiConfig: {e}", file=sys.stderr)
+    sys.exit(1)
