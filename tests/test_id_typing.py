d82ef1a9f947a8fd6670133138805dc0b4ec6941
matmaer <22435003+matmaer@users.noreply.github.com>
2025-07-11 00:20:28 +0200

    add test to see if all Enum member names are in use

--- tests/test_id_typing.py
+++ tests/test_id_typing.py
@@ -2,7 +2,7 @@
 
 import ast
 import inspect
-from enum import StrEnum
+from enum import Enum, StrEnum
 from pathlib import Path
 
 import pytest
@@ -41,8 +41,7 @@ def test_no_hardcoded_ids(py_file: Path):
         )
 
 
-def get_str_enum_members() -> list[StrEnum]:
-    """Helper to get all members of all StrEnum classes in id_typing."""
+def _get_str_enum_members() -> list[StrEnum]:
     members: list[StrEnum] = []
     for _, enum_class in inspect.getmembers(id_typing, inspect.isclass):
         if issubclass(enum_class, StrEnum) and enum_class is not StrEnum:
@@ -53,12 +52,42 @@ def get_str_enum_members() -> list[StrEnum]:
 
 @pytest.mark.parametrize(
     "str_enum_member",
-    get_str_enum_members(),
+    _get_str_enum_members(),
     ids=lambda member: f"{member.__class__.__name__}.{member.name}",
 )
 def test_str_enum_members_in_use(str_enum_member: StrEnum):
-    """Check if a StrEnum member is used in any of the source files."""
     search_term = str(str_enum_member.value)
+    found = False
+    for py_file in modules_to_test(exclude_file_names=["id_typing.py"]):
+        content = py_file.read_text()
+        if search_term in content:
+            found = True
+            break
+
+    if not found:
+        pytest.skip(
+            f"'{str_enum_member.name}' from {str_enum_member.__class__.__name__} "
+            "is not in use."
+        )
+
+
+def _get_enum_members() -> list[Enum]:
+
+    members: list[Enum] = []
+    for _, enum_class in inspect.getmembers(id_typing, inspect.isclass):
+        if issubclass(enum_class, Enum) and enum_class is not StrEnum:
+            for member in enum_class:
+                members.append(member)
+    return members
+
+
+@pytest.mark.parametrize(
+    "str_enum_member",
+    _get_enum_members(),
+    ids=lambda member: f"{member.__class__.__name__}.{member.name}",
+)
+def test_enum_members_in_use(str_enum_member: Enum):
+    search_term = str(str_enum_member.name)
     found = False
     for py_file in modules_to_test(exclude_file_names=["id_typing.py"]):
         content = py_file.read_text()
