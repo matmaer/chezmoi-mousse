f2572943589b50ed5000dd568351b5b3f7565d44
matmaer <22435003+matmaer@users.noreply.github.com>
2025-08-16 23:55:11 +0200

    improve tests

--- tests/test_chezmoi.py
+++ tests/test_chezmoi.py
@@ -1,46 +1,17 @@
+import ast
+
 import pytest
+from _test_utils import get_class_public_members, modules_to_test
 
 from chezmoi_mousse.chezmoi import Chezmoi, ManagedStatus
 
 exclude_files = ["chezmoi.py"]
 
 
-def _get_chezmoi_public_members() -> list[tuple[str, str]]:
-    import inspect
-
-    members: list[tuple[str, str]] = []
-    for name, member in inspect.getmembers(Chezmoi):
-        if not name.startswith("_"):
-            if isinstance(member, property):
-                members.append((name, "property"))
-            elif inspect.isfunction(member):
-                members.append((name, "method"))
-            elif not callable(member):
-                members.append((name, "attribute"))
-    return members
-
-
-def _get_managed_status_public_members() -> list[tuple[str, str]]:
-    import inspect
-
-    members: list[tuple[str, str]] = []
-    for name, member in inspect.getmembers(ManagedStatus):
-        if not name.startswith("_"):
-            if isinstance(member, property):
-                members.append((name, "property"))
-            elif inspect.isfunction(member):
-                members.append((name, "method"))
-    return members
-
-
 @pytest.mark.parametrize(
-    "member_name, member_type", _get_chezmoi_public_members()
+    "member_name, member_type", get_class_public_members(Chezmoi)
 )
 def test_chezmoi_member_in_use(member_name: str, member_type: str):
-    import ast
-
-    from _test_utils import modules_to_test
-
     is_used = False
 
     for py_file in modules_to_test(exclude_file_names=exclude_files):
@@ -59,13 +30,9 @@ def test_chezmoi_member_in_use(member_name: str, member_type: str):
 
 
 @pytest.mark.parametrize(
-    "member_name, member_type", _get_managed_status_public_members()
+    "member_name, member_type", get_class_public_members(ManagedStatus)
 )
 def test_managed_status_member_in_use(member_name: str, member_type: str):
-    import ast
-
-    from _test_utils import modules_to_test
-
     is_used = False
 
     # Exclude chezmoi.py from the search
