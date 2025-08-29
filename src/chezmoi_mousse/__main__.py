12e57493773c93ef9ddf28ac3223bd345dfb832d
matmaer <22435003+matmaer@users.noreply.github.com>
2025-08-29 17:02:16 +0200

    update docstring

--- src/chezmoi_mousse/__main__.py
+++ src/chezmoi_mousse/__main__.py
@@ -7,6 +7,14 @@ def main():
     app = ChezmoiGUI()
 
     if os.environ.get("CHEZMOI_MOUSSE_DEV") == "1":
+        """Patched app._handle_exception method which will save a stacktrace to
+        error.log in the source directory in case an exception occurs.
+
+        Do not set the CHEZMOI_MOUSSE_DEV environment variable unless you have
+        a development environment set up. This is for debugging purposes and is
+        not needed to run the application, on the contrary, it may cause
+        issues.
+        """
         import traceback
         from pathlib import Path
 
