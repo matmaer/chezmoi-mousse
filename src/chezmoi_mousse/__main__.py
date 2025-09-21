c02c771b40472eb08068da9d4774149a2b47f202
matmaer <22435003+matmaer@users.noreply.github.com>
2025-09-21 20:55:22 +0200

    seperate main() from __main__.py into app.py

--- src/chezmoi_mousse/__main__.py
+++ src/chezmoi_mousse/__main__.py
@@ -1,30 +1,4 @@
-import os
-
-from chezmoi_mousse.gui import ChezmoiGUI
-
-
-def main():
-    app = ChezmoiGUI()
-
-    if os.environ.get("MOUSSE_ENABLE_CHANGES") == "1":
-        """Patched app._handle_exception method which will save a stacktrace to
-        error.log in the source directory in case an exception occurs."""
-        import traceback
-        from pathlib import Path
-
-        original_handle_exception = app._handle_exception  # type: ignore[method-assign]
-        src_dir = Path(__file__).parent.parent
-
-        def patched_handle_exception(error: Exception):
-            with open(src_dir / "error.log", "w") as f:
-                traceback.print_exc(file=f)
-            original_handle_exception(error)
-
-        # monkey patch
-        app._handle_exception = patched_handle_exception  # type: ignore[method-assign]
-
-    app.run(inline=False, headless=False, mouse=True)
-
+from chezmoi_mousse.app import main
 
 if __name__ == "__main__":
-    main()
+    exit(main())
