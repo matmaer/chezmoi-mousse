a92fb9df308fcf65a6e04ca19ac29e0ec9bd3236
matmaer <22435003+matmaer@users.noreply.github.com>
2025-10-10 03:57:33 +0200

    new file:   src/chezmoi_mousse/_canvas_data.py

---
+++ src/chezmoi_mousse/_canvas_data.py
@@ -0,0 +1,14 @@
+from enum import Enum
+
+
+class Canvas(Enum):
+    add_tab = ""
+    apply_tab = ""
+    config_tab = ""
+    help_tab = ""
+    init_tab = ""
+    logs_tab = ""
+    re_add_tab = ""
+    install_help = ""
+    maximized = ""
+    operate = ""
