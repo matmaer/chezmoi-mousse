06c8bd5dd087360d33a070755715b96bcc1cdeba
matmaer <22435003+matmaer@users.noreply.github.com>
2025-10-26 16:24:30 +0100

    Generalize InfoStrings and Chars info borders for paths instead of files/dirs.

--- src/chezmoi_mousse/_chars.py
+++ src/chezmoi_mousse/_chars.py
@@ -4,19 +4,17 @@ __all__ = ["Chars"]
 
 
 class Chars(StrEnum):
-    add_file_info_border = f"local {'\u2014' * 3}\u2192 chezmoi"  # '\N{EM DASH}', '\N{RIGHTWARDS ARROW}'
-    apply_file_info_border = f"local \u2190{'\u2014' * 3} chezmoi"  # '\N{LEFTWARDS ARROW}', '\N{EM DASH}'
+    add_info_border = f"local {'\u2014' * 3}\u2192 chezmoi"  # '\N{EM DASH}', '\N{RIGHTWARDS ARROW}'
+    apply_info_border = f"local \u2190{'\u2014' * 3} chezmoi"  # '\N{LEFTWARDS ARROW}', '\N{EM DASH}'
     bullet = "\u2022"  # '\N{BULLET}'
     burger = "\u2261"  # '\N{IDENTICAL TO}'
     check_mark = "\u2714"  # '\N{HEAVY CHECK MARK}'
-    destroy_file_info_border = "\u274c destroy file \u274c"  # '\N{CROSS MARK}'
+    destroy_info_border = "\u274c destroy \u274c"  # '\N{CROSS MARK}'
     down_triangle = "\u25bc "  # '\N{BLACK DOWN-POINTING TRIANGLE}'
-    forget_file_info_border = (
-        "\u2716 forget file \u2716"  # '\N{HEAVY MULTIPLICATION X}'
-    )
+    forget_info_border = "\u2716 forget \u2716"  # '\N{HEAVY MULTIPLICATION X}'
     # gear = "\u2699"  # '\N{GEAR}'
     lower_3_8ths_block = "\u2583"  # "\N{LOWER THREE EIGHTHS BLOCK}"
-    re_add_file_info_border = f"local {'\u2014' * 3}\u2192 chezmoi"  # '\N{EM DASH}', '\N{RIGHTWARDS ARROW}'
+    re_add_info_border = f"local {'\u2014' * 3}\u2192 chezmoi"  # '\N{EM DASH}', '\N{RIGHTWARDS ARROW}'
     right_triangle = "\u25b8 "  # '\N{BLACK RIGHT-POINTING SMALL TRIANGLE}'
     warning_sign = "\u26a0"  # '\N{WARNING SIGN}'
     x_mark = "\u2716"  # '\N{HEAVY MULTIPLICATION X}'
--- src/chezmoi_mousse/gui/shared/operate_screen.py
+++ src/chezmoi_mousse/gui/shared/operate_screen.py
@@ -34,20 +34,16 @@ __all__ = ["OperateInfo", "OperateScreen"]
 
 
 class InfoStrings(StrEnum):
-    add_file = "[$text-primary]Path will be added to your chezmoi dotfile manager source state.[/]"
-    apply_file = "[$text-primary]The file in the destination directory will be modified.[/]"
-    # apply_dir = "[$text-primary]The directory in the destination directory will be modified.[/]"
+    add_path = "[$text-primary]The path will be added to your chezmoi dotfile manager source state.[/]"
+    apply_path = "[$text-primary]The path in the destination directory will be modified.[/]"
     auto_commit = f"[$text-warning]{Chars.warning_sign} Auto commit is enabled: files will also be committed.{Chars.warning_sign}[/]"
     autopush = f"[$text-warning]{Chars.warning_sign} Auto push is enabled: files will be pushed to the remote.{Chars.warning_sign}[/]"
-    destroy_file = "[$text-error]Permanently remove the file both from your home directory and chezmoi's source directory, make sure you have a backup![/]"
-    # destroy_dir = "[$text-error]Permanently remove the directory both from your home directory and chezmoi's source directory, make sure you have a backup![/]"
+    destroy_path = "[$text-error]Permanently remove the path both from your home directory and chezmoi's source directory, make sure you have a backup![/]"
     diff_color = f"[$text-success]+ green lines will be added[/]\n[$text-error]- red lines will be removed[/]\n[dim]{Chars.bullet} dimmed lines for context[/]"
-    forget_file = "[$text-primary]Remove the file from the source state, i.e. stop managing them.[/]"
-    # forget_dir = "[$text-primary]Remove the directory from the source state, i.e. stop managing them.[/]"
-    re_add_file = (
-        "[$text-primary]Overwrite the source state with current local file[/]"
+    forget_path = "[$text-primary]Remove the path from the source state, i.e. stop managing them.[/]"
+    re_add_path = (
+        "[$text-primary]Overwrite the source state with current local path[/]"
     )
-    # re_add_dir = "[$text-primary]Overwrite the source state with thecurrent local directory[/]"
 
 
 class OperateInfo(Static):
@@ -66,23 +62,35 @@ class OperateInfo(Static):
         self.border_subtitle = "escape to cancel"
         lines_to_write: list[str] = []
         # show command help and set its subtitle
-        if OperateBtn.apply_file == self.operate_btn:
-            lines_to_write.append(InfoStrings.apply_file.value)
-            self.border_subtitle = Chars.apply_file_info_border
-        elif OperateBtn.re_add_file == self.operate_btn:
-            lines_to_write.append(InfoStrings.re_add_file.value)
-            self.border_subtitle = Chars.re_add_file_info_border
-        elif OperateBtn.add_file == self.operate_btn:
-            lines_to_write.append(InfoStrings.add_file.value)
-            self.border_subtitle = Chars.add_file_info_border
-        elif OperateBtn.forget_file == self.operate_btn:
-            lines_to_write.append(InfoStrings.forget_file.value)
-            self.border_subtitle = Chars.forget_file_info_border
-        elif OperateBtn.destroy_file == self.operate_btn:
-            lines_to_write.append(InfoStrings.destroy_file.value)
-            self.border_subtitle = Chars.destroy_file_info_border
+        if self.operate_btn in (OperateBtn.apply_file, OperateBtn.apply_dir):
+            lines_to_write.append(InfoStrings.apply_path.value)
+            self.border_subtitle = Chars.apply_info_border
+        elif self.operate_btn in (
+            OperateBtn.re_add_file,
+            OperateBtn.re_add_dir,
+        ):
+            lines_to_write.append(InfoStrings.re_add_path.value)
+            self.border_subtitle = Chars.re_add_info_border
+        elif self.operate_btn in (OperateBtn.add_file, OperateBtn.add_dir):
+            lines_to_write.append(InfoStrings.add_path.value)
+            self.border_subtitle = Chars.add_info_border
+        elif self.operate_btn in (
+            OperateBtn.forget_file,
+            OperateBtn.forget_dir,
+        ):
+            lines_to_write.append(InfoStrings.forget_path.value)
+            self.border_subtitle = Chars.forget_info_border
+        elif self.operate_btn in (
+            OperateBtn.destroy_file,
+            OperateBtn.destroy_dir,
+        ):
+            lines_to_write.append(InfoStrings.destroy_path.value)
+            self.border_subtitle = Chars.destroy_info_border
         # show git auto warnings
-        if not OperateBtn.apply_file == self.operate_btn:
+        if self.operate_btn not in (
+            OperateBtn.apply_file,
+            OperateBtn.apply_dir,
+        ):
             assert (
                 self.git_autocommit is not None
                 and self.git_autopush is not None
@@ -92,9 +100,11 @@ class OperateInfo(Static):
             if self.git_autopush is True:
                 lines_to_write.append(InfoStrings.autopush.value)
         # show git diff color info
-        if (
-            OperateBtn.apply_file == self.operate_btn
-            or OperateBtn.re_add_file == self.operate_btn
+        if self.operate_btn in (
+            OperateBtn.apply_file,
+            OperateBtn.apply_dir,
+            OperateBtn.re_add_file,
+            OperateBtn.re_add_dir,
         ):
             lines_to_write.append(InfoStrings.diff_color.value)
         lines_to_write.append(
