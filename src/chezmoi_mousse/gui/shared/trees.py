962d44d33c1554509994b1eb20ab88978666eb23
matmaer <22435003+matmaer@users.noreply.github.com>
2025-10-31 15:19:20 +0100

    Add todo,  logic error to correct.

--- src/chezmoi_mousse/gui/shared/trees.py
+++ src/chezmoi_mousse/gui/shared/trees.py
@@ -198,6 +198,8 @@ class TreeBase(Tree[NodeData], AppType):
     def add_status_dirs_in(self, *, tree_node: TreeNode[NodeData]) -> None:
         assert tree_node.data is not None
 
+        # TODO: correct logic when it comes to the parent condition
+
         if self.ids.canvas_name == Canvas.apply:
             result: "PathDict" = {
                 path: status
