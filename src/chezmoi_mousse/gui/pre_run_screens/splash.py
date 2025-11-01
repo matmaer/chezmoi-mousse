19b7a9ddf0dfb125236e9d8a4757d7f6e34eda58
matmaer <22435003+matmaer@users.noreply.github.com>
2025-11-01 02:34:19 +0100

    First run dump-config and then continue. Prepare for proper handling of destDir.

--- src/chezmoi_mousse/gui/pre_run_screens/splash.py
+++ src/chezmoi_mousse/gui/pre_run_screens/splash.py
@@ -81,7 +81,6 @@ FADE_HEIGHT = len(SPLASH)
 FADE_WIDTH = len(max(SPLASH, key=len))
 LOG_PADDING_WIDTH = 37
 LOADED_SUFFIX = "loaded"
-NOT_FOUND_SUFFIX = "not found"
 
 
 def create_deque() -> deque[Style]:
@@ -139,16 +138,7 @@ class LoadingScreen(Screen[SplashData | None], AppType):
 
     @work(thread=True, group="io_workers")
     def run_read_cmd(self, splash_cmd: ReadCmd) -> None:
-
         splash_log = self.query_exactly_one(RichLog)
-
-        if not self.chezmoi_found:
-            cmd_text = "chezmoi command"
-            padding = LOG_PADDING_WIDTH - len(cmd_text)
-            log_text = f"{cmd_text} {'.' * padding} {NOT_FOUND_SUFFIX}"
-            splash_log.write(log_text)
-            return
-
         cmd_result: "CommandResult" = self.app.chezmoi.read(splash_cmd)
         globals()[splash_cmd.name] = cmd_result
         cmd_text = cmd_result.pretty_cmd.replace(
@@ -157,15 +147,23 @@ class LoadingScreen(Screen[SplashData | None], AppType):
         padding = LOG_PADDING_WIDTH - len(cmd_text)
         log_text = f"{cmd_text} {'.' * padding} {LOADED_SUFFIX}"
         splash_log.write(log_text)
-        if splash_cmd.name == ReadCmd.dump_config.name:
-            parsed_config = json.loads(cmd_result.std_out)
-            globals()["parsed_config"] = ParsedConfig(
-                dest_dir=Path(parsed_config["destDir"]),
-                git_autoadd=parsed_config["git"]["autoadd"],
-                source_dir=Path(parsed_config["sourceDir"]),
-                git_autocommit=parsed_config["git"]["autocommit"],
-                git_autopush=parsed_config["git"]["autopush"],
-            )
+
+    @work
+    async def run_dump_config_cmd(self, splash_cmd: ReadCmd) -> None:
+        splash_log = self.query_exactly_one(RichLog)
+        cmd_result: "CommandResult" = self.app.chezmoi.read(splash_cmd)
+        globals()[splash_cmd.name] = cmd_result
+        padding = LOG_PADDING_WIDTH - len(cmd_result.pretty_cmd)
+        log_text = f"{cmd_result.pretty_cmd} {'.' * padding} {LOADED_SUFFIX}"
+        splash_log.write(log_text)
+        parsed_config = json.loads(cmd_result.std_out)
+        globals()["parsed_config"] = ParsedConfig(
+            dest_dir=Path(parsed_config["destDir"]),
+            git_autoadd=parsed_config["git"]["autoadd"],
+            source_dir=Path(parsed_config["sourceDir"]),
+            git_autocommit=parsed_config["git"]["autocommit"],
+            git_autopush=parsed_config["git"]["autopush"],
+        )
 
     def all_workers_finished(self) -> None:
         if all(
@@ -196,7 +194,7 @@ class LoadingScreen(Screen[SplashData | None], AppType):
                 )
             )
 
-    def on_mount(self) -> None:
+    async def on_mount(self) -> None:
         middle_container = self.query_one(Middle)
         middle_container.styles.width = FADE_WIDTH
         animated_fade = self.query_exactly_one(AnimatedFade)
@@ -205,19 +203,27 @@ class LoadingScreen(Screen[SplashData | None], AppType):
         rich_log.styles.width = "auto"
         rich_log.styles.color = "#6DB2FF"
         rich_log.styles.margin = 2
-        if self.chezmoi_found:
-            rich_log.styles.height = len(SPLASH_COMMANDS)
-        else:
-            rich_log.styles.height = 1
         self.all_workers_timer = self.set_interval(
             interval=1, callback=self.all_workers_finished
         )
         self.fade_timer = self.set_interval(
             interval=0.05, callback=animated_fade.refresh
         )
-        if not self.chezmoi_found:
-            self.run_read_cmd("")
-            return
+        if self.chezmoi_found is True:
+            rich_log.styles.height = len(SPLASH_COMMANDS)
         else:
-            for cmd in SPLASH_COMMANDS:
-                self.run_read_cmd(cmd)
+            rich_log.styles.height = 1
+            splash_log = self.query_exactly_one(RichLog)
+            cmd_text = "chezmoi command"
+            padding = LOG_PADDING_WIDTH - len(cmd_text)
+            log_text = f"{cmd_text} {'.' * padding} not found"
+            splash_log.write(log_text)
+            return
+
+        dump_worker = self.run_dump_config_cmd(
+            SPLASH_COMMANDS.pop(SPLASH_COMMANDS.index(ReadCmd.dump_config))
+        )
+        await dump_worker.wait()
+
+        for cmd in SPLASH_COMMANDS:
+            self.run_read_cmd(cmd)
