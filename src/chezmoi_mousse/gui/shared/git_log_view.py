fb5af484d75d40756543b396ff0f6eae45a625c9
matmaer <22435003+matmaer@users.noreply.github.com>
2025-11-05 03:24:47 +0100

    Implement GitLogView as a container.

--- src/chezmoi_mousse/gui/shared/git_log_view.py
+++ src/chezmoi_mousse/gui/shared/git_log_view.py
@@ -1,8 +1,11 @@
 from collections.abc import Iterable
+from enum import StrEnum
 from pathlib import Path
 from typing import TYPE_CHECKING
 
 from rich.text import Text
+from textual.app import ComposeResult
+from textual.containers import Vertical
 from textual.reactive import reactive
 from textual.widgets import DataTable
 
@@ -13,22 +16,37 @@ if TYPE_CHECKING:
 
     from .canvas_ids import CanvasIds
 
+    DataTableText = DataTable[Text]
+else:
+    DataTableText = DataTable
+
 __all__ = ["GitLogView"]
 
 
-class GitLogView(DataTable[Text], AppType):
+class Strings(StrEnum):
+    click_file_path = "Click a file path in the tree to see the contents."
+
+
+class GitLogView(Vertical, AppType):
 
     destDir: "Path | None" = None
     path: reactive[Path | None] = reactive(None, init=False)
 
     def __init__(self, *, ids: "CanvasIds") -> None:
-        self.ids = ids
         super().__init__(
-            id=self.ids.view_id(view=ViewName.git_log_view), show_cursor=False
+            id=ids.view_id(view=ViewName.git_log_view),
+            classes=Tcss.border_title_top.name,
         )
-        self.click_file_path = Text(
-            "Click a file path in the tree to see the contents.", style="dim"
+        self.ids = ids
+        self.git_log_table_id = ids.datatable_id(
+            view_name=ViewName.git_log_view
         )
+        self.git_log_table_qid = ids.datatable_id(
+            "#", view_name=ViewName.git_log_view
+        )
+
+    def compose(self) -> ComposeResult:
+        yield DataTable(id=self.git_log_table_id, show_cursor=False)
 
     def on_mount(self) -> None:
         if self.ids.canvas_name in (
@@ -36,12 +54,11 @@ class GitLogView(DataTable[Text], AppType):
             CanvasName.apply_tab,
             CanvasName.re_add_tab,
         ):
-            self.add_class(Tcss.border_title_top.name)
-            self.border_title = f" {self.destDir} "
-            self.add_columns(
+            datatable = self.query_one(self.git_log_table_qid, DataTableText)
+            datatable.add_columns(
                 Text('This is the destination directory "chezmoi destDir"')
             )
-            self.add_row(self.click_file_path)
+            datatable.add_row(Text(Strings.click_file_path))
             return
         if self.ids.canvas_name == CanvasName.logs_tab:
             git_log_result: "CommandResult" = self.app.chezmoi.read(
@@ -50,14 +67,16 @@ class GitLogView(DataTable[Text], AppType):
             self.populate_data_table(git_log_result.std_out)
 
     def _add_row_with_style(self, columns: list[str], style: str) -> None:
+        datatable = self.query_one(self.git_log_table_qid, DataTableText)
         row: Iterable[Text] = [
             Text(cell_text, style=style) for cell_text in columns
         ]
-        self.add_row(*row)
+        datatable.add_row(*row)
 
     def populate_data_table(self, cmd_output: str) -> None:
-        self.clear(columns=True)
-        self.add_columns("COMMIT", "MESSAGE")
+        datatable = self.query_one(self.git_log_table_qid, DataTableText)
+        datatable.clear(columns=True)
+        datatable.add_columns("COMMIT", "MESSAGE")
         styles = {
             "ok": self.app.theme_variables["text-success"],
             "warning": self.app.theme_variables["text-warning"],
@@ -72,17 +91,15 @@ class GitLogView(DataTable[Text], AppType):
             elif columns[1].split(maxsplit=1)[0] == "Remove":
                 self._add_row_with_style(columns, styles["error"])
             else:
-                self.add_row(*(Text(cell) for cell in columns))
+                datatable.add_row(*(Text(cell) for cell in columns))
 
     def watch_path(self) -> None:
         if self.path is None:
             return
-        self.border_title = f" {self.path} "
         source_path_str: str = self.app.chezmoi.read(
             ReadCmd.source_path, self.path
         ).std_out
         git_log_result: "CommandResult" = self.app.chezmoi.read(
             ReadCmd.git_log, Path(source_path_str)
         )
-        self.border_title = f" {self.path} "
         self.populate_data_table(git_log_result.std_out)
