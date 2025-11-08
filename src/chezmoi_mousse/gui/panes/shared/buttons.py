e8ec8fdc55eaf86af616dc2a5b7bd64187534116
matmaer <22435003+matmaer@users.noreply.github.com>
2025-11-09 00:16:22 +0100

    Needed to properly center the buttons.

--- src/chezmoi_mousse/gui/panes/shared/buttons.py
+++ src/chezmoi_mousse/gui/panes/shared/buttons.py
@@ -61,15 +61,14 @@ class OperateButton(Vertical):
         super().__init__()
 
     def compose(self) -> ComposeResult:
-        yield Vertical(
-            Button(
+        with Vertical(classes=Tcss.single_button_vertical.name):
+            yield Button(
                 label=self.button_enum.initial_label,
                 id=self.ids.button_id(btn=self.button_enum),
                 classes=Tcss.operate_button.name,
                 disabled=True,
                 tooltip=self.button_enum.initial_tooltip,
             )
-        )
 
 
 class NavButtonsVertical(VerticalGroup):
@@ -137,13 +136,12 @@ class TabBtnHorizontal(HorizontalGroup):
 
     def compose(self) -> ComposeResult:
         for button_enum in self.buttons:
-            yield Vertical(
-                Button(
+            with Vertical(classes=Tcss.single_button_vertical.name):
+                yield Button(
                     label=button_enum.value,
                     id=self.ids.button_id(btn=button_enum),
                     classes=Tcss.tab_button.name,
                 )
-            )
 
     def on_mount(self) -> None:
         buttons = self.query(Button)
