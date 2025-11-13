52cf91d146ade9762673f97f7c2e4ca5f15c676c
matmaer <22435003+matmaer@users.noreply.github.com>
2025-11-13 14:23:10 +0100

    Inherit from Static for the SectionHeader class

--- src/chezmoi_mousse/gui/tabs/shared/section_headers.py
+++ src/chezmoi_mousse/gui/tabs/shared/section_headers.py
@@ -1,10 +1,12 @@
 from enum import StrEnum
 from typing import TYPE_CHECKING
 
+from textual.app import ComposeResult
+from textual.containers import Vertical
 from textual.content import Content
-from textual.widgets import Label, RichLog
+from textual.widgets import Label, Static
 
-from chezmoi_mousse import CanvasName, Tcss, ViewName
+from chezmoi_mousse import Tcss, ViewName
 
 __all__ = ["InitialHeader", "SectionHeader", "SectionLabel", "SectionSubLabel"]
 
@@ -12,11 +14,16 @@ if TYPE_CHECKING:
     from .canvas_ids import CanvasIds
 
 
-class Sections(StrEnum):
+class SectionStrings(StrEnum):
+    path_info = "Path Information"
     in_dest_dir = 'This is the destination directory "chezmoi destDir"'
     initial_git_log_msg = (
         'Click a path in the tree to see the output from "chezmoi git log".'
     )
+    initial_diff_msg = (
+        'Click a path in the tree to see the output from "chezmoi diff".'
+    )
+    initial_contents_msg = "Click a path in the tree to see the file contents."
 
 
 class SectionLabel(Label):
@@ -31,44 +38,40 @@ class SectionSubLabel(Label):
         super().__init__(label_text, classes=Tcss.section_header.name)
 
 
-class SectionHeader(RichLog):
+class SectionHeader(Static):
     def __init__(
-        self,
-        ids: "CanvasIds",
-        view_name: ViewName,
-        messages: list[Content],
-        initial: bool = False,
+        self, ids: "CanvasIds", view_name: ViewName, messages: list[Content]
     ) -> None:
         self.messages = messages
-        if initial:
-            self.super_id = ids.initial_header_id(view_name=view_name)
-        else:
-            self.super_id = ids.section_header_id(view_name=view_name)
-
         super().__init__(
-            id=self.super_id,
-            auto_scroll=False,
-            highlight=True,
-            wrap=True,  # TODO: implement footer binding to toggle wrap
+            id=ids.section_header_id(view_name=view_name),
             markup=True,
             classes=Tcss.section_header.name,
         )
 
 
-class InitialHeader(SectionHeader):
+class InitialHeader(Vertical):
     def __init__(self, ids: "CanvasIds", view_name: ViewName) -> None:
+        super().__init__(classes=Tcss.section_header.name)
         self.ids = ids
         self.view_name = view_name
-        super().__init__(
-            ids=ids, view_name=view_name, messages=[], initial=True
+        self.static_id = self.ids.initial_header_id(view_name=self.view_name)
+        self.static_qid = self.ids.initial_header_id(
+            "#", view_name=self.view_name
         )
 
+    def compose(self) -> ComposeResult:
+        yield SectionSubLabel(SectionStrings.path_info)
+        yield Static("", id=self.static_id)
+
     def on_mount(self) -> None:
-        self.add_class(Tcss.rich_header.name)
-        self.write(Content(Sections.in_dest_dir))
-        if (
-            self.ids.canvas_name
-            in (CanvasName.apply_tab, CanvasName.re_add_tab)
-            and self.view_name == ViewName.git_log_view
-        ):
-            self.write(Content(Sections.initial_git_log_msg))
+        static_widget = self.query_one(self.static_qid, Static)
+        lines_to_add: list[str] = []
+        lines_to_add.append(SectionStrings.in_dest_dir)
+        if self.view_name == ViewName.diff_view:
+            lines_to_add.append(SectionStrings.initial_diff_msg)
+        elif self.view_name == ViewName.contents_view:
+            lines_to_add.append(SectionStrings.initial_contents_msg)
+        elif self.view_name == ViewName.git_log_view:
+            lines_to_add.append(SectionStrings.initial_git_log_msg)
+        static_widget.update("\n".join(lines_to_add))
