571bd3d4964429ccefca9e61c24f08dee1dbac4a
matmaer <22435003+matmaer@users.noreply.github.com>
2025-11-14 19:49:27 +0100

    Export CatConfigOutput

--- src/chezmoi_mousse/shared/_cat_config.py
+++ src/chezmoi_mousse/shared/_cat_config.py
@@ -10,6 +10,8 @@ from chezmoi_mousse.shared import MainSectionLabelText, SectionLabel
 if TYPE_CHECKING:
     from chezmoi_mousse import CanvasIds
 
+__all__ = ["CatConfigOutput"]
+
 
 class CatConfigOutput(Vertical):
     def __init__(self, ids: "CanvasIds"):
