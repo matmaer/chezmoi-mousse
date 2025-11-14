15d4494c0024091ad7e2fd9ee804d05dcc4a1996
matmaer <22435003+matmaer@users.noreply.github.com>
2025-11-14 21:30:21 +0100

    Implement InitScreen

--- src/chezmoi_mousse/gui/init_screen.py
+++ src/chezmoi_mousse/gui/init_screen.py
@@ -1,10 +1,17 @@
 from typing import TYPE_CHECKING
 
 from textual.app import ComposeResult
+from textual.containers import ScrollableContainer, Vertical
 from textual.screen import Screen
-from textual.widgets import Static
+from textual.widgets import ContentSwitcher, Pretty
 
-from chezmoi_mousse import SplashData
+from chezmoi_mousse import ContainerName, FlatBtn, SplashData, Tcss, ViewName
+from chezmoi_mousse.shared import (
+    DoctorTable,
+    FlatButtonsVertical,
+    MainSectionLabelText,
+    SectionLabel,
+)
 
 __all__ = ["InitScreen"]
 
@@ -12,15 +19,60 @@ if TYPE_CHECKING:
     from chezmoi_mousse import CanvasIds
 
 
+class InitSwitcher(ContentSwitcher):
+
+    def __init__(self, ids: "CanvasIds"):
+        self.ids = ids
+        self.doctor_list_view_id = self.ids.view_id(view=ViewName.doctor_view)
+        super().__init__(
+            id=self.ids.content_switcher_id(
+                name=ContainerName.init_screen_switcher
+            ),
+            initial=self.doctor_list_view_id,
+        )
+        self.doctor_table_qid = ids.datatable_id(
+            "#", view_name=ViewName.doctor_view
+        )
+
+    def compose(self) -> ComposeResult:
+        yield ScrollableContainer(
+            SectionLabel(MainSectionLabelText.doctor_output),
+            DoctorTable(ids=self.ids),
+        )
+        yield ScrollableContainer(
+            SectionLabel(MainSectionLabelText.cat_config_output),
+            Pretty("<cat-config>", id=ViewName.pretty_cat_config_view),
+            id=self.ids.view_id(view=ViewName.cat_config_view),
+        )
+        yield ScrollableContainer(
+            SectionLabel(MainSectionLabelText.template_data_output),
+            Pretty("<template_data>", id=ViewName.pretty_template_data_view),
+            id=self.ids.view_id(view=ViewName.template_data_view),
+        )
+
+
 class InitScreen(Screen[None]):
 
     def __init__(
         self, *, ids: "CanvasIds", commands_data: "SplashData"
     ) -> None:
-        super().__init__()
+        self.ids = ids
+        super().__init__(id=ids.canvas_name, classes=Tcss.screen_base.name)
         self.commands_data = commands_data
+        self.tab_vertical_id = ids.tab_vertical_id(
+            name=ContainerName.left_side
+        )
 
     def compose(self) -> ComposeResult:
-        yield Static("Placeholder for chezmoi init screen")
-        yield Static("Commands Data:")
-        yield Static(f"{dir(self.commands_data)}")
+        with Vertical(
+            id=self.tab_vertical_id, classes=Tcss.tab_left_vertical.name
+        ):
+            yield FlatButtonsVertical(
+                ids=self.ids,
+                buttons=(
+                    FlatBtn.doctor,
+                    FlatBtn.cat_config,
+                    FlatBtn.template_data,
+                ),
+            )
+        yield InitSwitcher(self.ids)
