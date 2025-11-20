907e0a49713eb8eb96d9092eda5c9f24e0d2dd5f
matmaer <22435003+matmaer@users.noreply.github.com>
2025-11-20 02:33:17 +0100

    Add in_dest_dir_logger name.

--- src/chezmoi_mousse/_names.py
+++ src/chezmoi_mousse/_names.py
@@ -65,6 +65,7 @@ class LogName(StrEnum):
     contents_logger = auto()
     debug_logger = auto()
     diff_logger = auto()
+    in_dest_dir_logger = auto()
     splash_logger = auto()
     operate_logger = auto()
     read_logger = auto()
