4be7a924f70da881c6b315185291c1f281f3b90c
matmaer <22435003+matmaer@users.noreply.github.com>
2025-11-23 21:42:23 +0100

    Rename canvas_ids to ids for all Ids classes.

--- src/chezmoi_mousse/_app_ids.py
+++ src/chezmoi_mousse/_app_ids.py
@@ -97,68 +97,56 @@ class AppIds:
 
 
 class ContainerIds:
-    def __init__(self, canvas_ids: AppIds):
-        self.dest_dir_info = canvas_ids.container_id(
-            name=ContainerName.dest_dir_info
-        )
+    def __init__(self, ids: AppIds):
+        self.dest_dir_info = ids.container_id(name=ContainerName.dest_dir_info)
         self.dest_dir_info_q = f"#{self.dest_dir_info}"
-        self.doctor = canvas_ids.container_id(name=ContainerName.doctor)
+        self.doctor = ids.container_id(name=ContainerName.doctor)
         self.doctor_q = f"#{self.doctor}"
-        self.git_log_path = canvas_ids.container_id(
-            name=ContainerName.git_log_path
-        )
+        self.git_log_path = ids.container_id(name=ContainerName.git_log_path)
         self.git_log_path_q = f"#{self.git_log_path}"
-        self.git_log_global = canvas_ids.container_id(
+        self.git_log_global = ids.container_id(
             name=ContainerName.git_log_global
         )
         self.git_log_global_q = f"#{self.git_log_global}"
-        self.left_side = canvas_ids.container_id(name=ContainerName.left_side)
+        self.left_side = ids.container_id(name=ContainerName.left_side)
         self.left_side_q = f"#{self.left_side}"
-        self.operate_buttons = canvas_ids.container_id(
+        self.operate_buttons = ids.container_id(
             name=ContainerName.operate_buttons
         )
         self.operate_buttons_q = f"#{self.operate_buttons}"
-        self.post_operate = canvas_ids.container_id(
-            name=ContainerName.post_operate
-        )
+        self.post_operate = ids.container_id(name=ContainerName.post_operate)
         self.post_operate_q = f"#{self.post_operate}"
-        self.pre_operate = canvas_ids.container_id(
-            name=ContainerName.pre_operate
-        )
+        self.pre_operate = ids.container_id(name=ContainerName.pre_operate)
         self.pre_operate_q = f"#{self.pre_operate}"
-        self.right_side = canvas_ids.container_id(
-            name=ContainerName.right_side
-        )
+        self.right_side = ids.container_id(name=ContainerName.right_side)
         self.right_side_q = f"#{self.right_side}"
-        self.switch_slider = canvas_ids.container_id(
-            name=ContainerName.switch_slider
-        )
+        self.switch_slider = ids.container_id(name=ContainerName.switch_slider)
         self.switch_slider_q = f"#{self.switch_slider}"
 
 
 class ContentSwitcherIds:
-    def __init__(self, canvas_ids: AppIds):
-        self.config_tab = canvas_ids.content_switcher_id(
+    def __init__(self, ids: AppIds):
+        self.config_tab = ids.content_switcher_id(
             switcher=ContentSwitcherName.config_switcher
         )
         self.config_tab_q = f"#{self.config_tab}"
-        self.help_tab = canvas_ids.content_switcher_id(
+        self.help_tab = ids.content_switcher_id(
             switcher=ContentSwitcherName.help_switcher
         )
         self.help_tab_q = f"#{self.help_tab}"
-        self.init_screen = canvas_ids.content_switcher_id(
+        self.init_screen = ids.content_switcher_id(
             switcher=ContentSwitcherName.init_screen_switcher
         )
         self.init_screen_q = f"#{self.init_screen}"
-        self.logs_tab = canvas_ids.content_switcher_id(
+        self.logs_tab = ids.content_switcher_id(
             switcher=ContentSwitcherName.logs_switcher
         )
         self.logs_tab_q = f"#{self.logs_tab}"
-        self.trees = canvas_ids.content_switcher_id(
+        self.trees = ids.content_switcher_id(
             switcher=ContentSwitcherName.tree_switcher
         )
         self.trees_q = f"#{self.trees}"
-        self.views = canvas_ids.content_switcher_id(
+        self.views = ids.content_switcher_id(
             switcher=ContentSwitcherName.view_switcher
         )
         self.views_q = f"#{self.views}"
@@ -167,144 +155,136 @@ class ContentSwitcherIds:
 class DataTableIds:
     """DataTable widget their id's."""
 
-    def __init__(self, canvas_ids: AppIds):
-        self.doctor = canvas_ids.datatable_id(
+    def __init__(self, ids: AppIds):
+        self.doctor = ids.datatable_id(
             datatable_name=DataTableName.doctor_table
         )
         self.doctor_q = f"#{self.doctor}"
-        self.git_log = canvas_ids.datatable_id(
+        self.git_log = ids.datatable_id(
             datatable_name=DataTableName.git_log_table
         )
         self.git_log_q = f"#{self.git_log}"
 
 
 class FilterIds:
-    def __init__(self, canvas_ids: AppIds):
-        self.expand_all = canvas_ids.switch_id(switch=Switches.expand_all)
+    def __init__(self, ids: AppIds):
+        self.expand_all = ids.switch_id(switch=Switches.expand_all)
         self.expand_all_q = f"#{self.expand_all}"
-        self.unchanged = canvas_ids.switch_id(switch=Switches.unchanged)
+        self.unchanged = ids.switch_id(switch=Switches.unchanged)
         self.unchanged_q = f"#{self.unchanged}"
-        self.unmanaged_dirs = canvas_ids.switch_id(
-            switch=Switches.unmanaged_dirs
-        )
+        self.unmanaged_dirs = ids.switch_id(switch=Switches.unmanaged_dirs)
         self.unmanaged_dirs_q = f"#{self.unmanaged_dirs}"
-        self.unwanted = canvas_ids.switch_id(switch=Switches.unwanted)
+        self.unwanted = ids.switch_id(switch=Switches.unwanted)
         self.unwanted_q = f"#{self.unwanted}"
 
 
 class FlatButtonIds:
-    def __init__(self, canvas_ids: AppIds):
-        self.add_help = canvas_ids.flat_button_id(btn=FlatBtn.add_help)
+    def __init__(self, ids: AppIds):
+        self.add_help = ids.flat_button_id(btn=FlatBtn.add_help)
         self.add_help_q = f"#{self.add_help}"
-        self.apply_help = canvas_ids.flat_button_id(btn=FlatBtn.apply_help)
+        self.apply_help = ids.flat_button_id(btn=FlatBtn.apply_help)
         self.apply_help_q = f"#{self.apply_help}"
-        self.cat_config = canvas_ids.flat_button_id(btn=FlatBtn.cat_config)
+        self.cat_config = ids.flat_button_id(btn=FlatBtn.cat_config)
         self.cat_config_q = f"#{self.cat_config}"
-        self.clone_repo = canvas_ids.flat_button_id(btn=FlatBtn.clone_repo)
+        self.clone_repo = ids.flat_button_id(btn=FlatBtn.clone_repo)
         self.clone_repo_q = f"#{self.clone_repo}"
-        self.diagram = canvas_ids.flat_button_id(btn=FlatBtn.diagram)
+        self.diagram = ids.flat_button_id(btn=FlatBtn.diagram)
         self.diagram_q = f"#{self.diagram}"
-        self.doctor = canvas_ids.flat_button_id(btn=FlatBtn.doctor)
+        self.doctor = ids.flat_button_id(btn=FlatBtn.doctor)
         self.doctor_q = f"#{self.doctor}"
-        self.exit_app = canvas_ids.flat_button_id(btn=FlatBtn.exit_app)
+        self.exit_app = ids.flat_button_id(btn=FlatBtn.exit_app)
         self.exit_app_q = f"#{self.exit_app}"
-        self.ignored = canvas_ids.flat_button_id(btn=FlatBtn.ignored)
+        self.ignored = ids.flat_button_id(btn=FlatBtn.ignored)
         self.ignored_q = f"#{self.ignored}"
-        self.new_repo = canvas_ids.flat_button_id(btn=FlatBtn.new_repo)
+        self.new_repo = ids.flat_button_id(btn=FlatBtn.new_repo)
         self.new_repo_q = f"#{self.new_repo}"
-        self.pw_mgr_info = canvas_ids.flat_button_id(btn=FlatBtn.pw_mgr_info)
+        self.pw_mgr_info = ids.flat_button_id(btn=FlatBtn.pw_mgr_info)
         self.pw_mgr_info_q = f"#{self.pw_mgr_info}"
-        self.re_add_help = canvas_ids.flat_button_id(btn=FlatBtn.re_add_help)
+        self.re_add_help = ids.flat_button_id(btn=FlatBtn.re_add_help)
         self.re_add_help_q = f"#{self.re_add_help}"
-        self.template_data = canvas_ids.flat_button_id(
-            btn=FlatBtn.template_data
-        )
+        self.template_data = ids.flat_button_id(btn=FlatBtn.template_data)
         self.template_data_q = f"#{self.template_data}"
 
 
 class LoggerIds:
     """RichLog widgets their id's."""
 
-    def __init__(self, canvas_ids: AppIds):
-        self.app = canvas_ids.view_id(view=LogName.app_logger)
+    def __init__(self, ids: AppIds):
+        self.app = ids.view_id(view=LogName.app_logger)
         self.app_q = f"#{self.app}"
-        self.contents = canvas_ids.view_id(view=LogName.contents_logger)
+        self.contents = ids.view_id(view=LogName.contents_logger)
         self.contents_q = f"#{self.contents}"
-        self.debug = canvas_ids.view_id(view=LogName.debug_logger)
+        self.debug = ids.view_id(view=LogName.debug_logger)
         self.debug_q = f"#{self.debug}"
-        self.diff = canvas_ids.view_id(view=LogName.diff_logger)
+        self.diff = ids.view_id(view=LogName.diff_logger)
         self.diff_q = f"#{self.diff}"
-        self.operate = canvas_ids.view_id(view=LogName.operate_logger)
+        self.operate = ids.view_id(view=LogName.operate_logger)
         self.operate_q = f"#{self.operate}"
-        self.read = canvas_ids.view_id(view=LogName.read_logger)
+        self.read = ids.view_id(view=LogName.read_logger)
         self.read_q = f"#{self.read}"
-        self.splash = canvas_ids.view_id(view=LogName.splash_logger)
+        self.splash = ids.view_id(view=LogName.splash_logger)
         self.splash_q = f"#{self.splash}"
 
 
 class TabButtonIds:
     """Buttons used by ContentSwitcher classes to switch views."""
 
-    def __init__(self, canvas_ids: AppIds):
-        self.app_log = canvas_ids.tab_button_id(btn=TabBtn.app_log)
-        self.contents = canvas_ids.tab_button_id(btn=TabBtn.contents)
-        self.debug_log = canvas_ids.tab_button_id(btn=TabBtn.debug_log)
-        self.diff = canvas_ids.tab_button_id(btn=TabBtn.diff)
-        self.git_log = canvas_ids.tab_button_id(btn=TabBtn.git_log_path)
-        self.git_log_global = canvas_ids.tab_button_id(
-            btn=TabBtn.git_log_global
-        )
-        self.list = canvas_ids.tab_button_id(btn=TabBtn.list)
-        self.operate_log = canvas_ids.tab_button_id(btn=TabBtn.operate_log)
-        self.read_log = canvas_ids.tab_button_id(btn=TabBtn.read_log)
-        self.tree = canvas_ids.tab_button_id(btn=TabBtn.tree)
+    def __init__(self, ids: AppIds):
+        self.app_log = ids.tab_button_id(btn=TabBtn.app_log)
+        self.contents = ids.tab_button_id(btn=TabBtn.contents)
+        self.debug_log = ids.tab_button_id(btn=TabBtn.debug_log)
+        self.diff = ids.tab_button_id(btn=TabBtn.diff)
+        self.git_log = ids.tab_button_id(btn=TabBtn.git_log_path)
+        self.git_log_global = ids.tab_button_id(btn=TabBtn.git_log_global)
+        self.list = ids.tab_button_id(btn=TabBtn.list)
+        self.operate_log = ids.tab_button_id(btn=TabBtn.operate_log)
+        self.read_log = ids.tab_button_id(btn=TabBtn.read_log)
+        self.tree = ids.tab_button_id(btn=TabBtn.tree)
 
 
 class TreeIds:
     """Tree widget their id's."""
 
-    def __init__(self, canvas_ids: AppIds):
-        self.dir_tree = canvas_ids.tree_id(tree=TreeName.dir_tree)
+    def __init__(self, ids: AppIds):
+        self.dir_tree = ids.tree_id(tree=TreeName.dir_tree)
         self.dir_tree_q = f"#{self.dir_tree}"
-        self.expanded = canvas_ids.tree_id(tree=TreeName.expanded_tree)
+        self.expanded = ids.tree_id(tree=TreeName.expanded_tree)
         self.expanded_q = f"#{self.expanded}"
-        self.list = canvas_ids.tree_id(tree=TreeName.list_tree)
+        self.list = ids.tree_id(tree=TreeName.list_tree)
         self.list_q = f"#{self.list}"
-        self.managed = canvas_ids.tree_id(tree=TreeName.managed_tree)
+        self.managed = ids.tree_id(tree=TreeName.managed_tree)
         self.managed_q = f"#{self.managed}"
 
 
 class ViewIds:
     """View Container id's used by ContentSwitcher classes."""
 
-    def __init__(self, canvas_ids: AppIds):
+    def __init__(self, ids: AppIds):
 
         # Help tab
-        self.add_help = canvas_ids.view_id(view=ViewName.add_help_view)
+        self.add_help = ids.view_id(view=ViewName.add_help_view)
         self.add_help_q = f"#{self.add_help}"
-        self.apply_help = canvas_ids.view_id(view=ViewName.apply_help_view)
+        self.apply_help = ids.view_id(view=ViewName.apply_help_view)
         self.apply_help_q = f"#{self.apply_help}"
-        self.re_add_help = canvas_ids.view_id(view=ViewName.re_add_help_view)
+        self.re_add_help = ids.view_id(view=ViewName.re_add_help_view)
         self.re_add_help_q = f"#{self.re_add_help}"
-        self.diagram = canvas_ids.view_id(view=ViewName.diagram_view)
+        self.diagram = ids.view_id(view=ViewName.diagram_view)
         self.diagram_q = f"#{self.diagram}"
 
         # Init screen
-        self.clone_repo = canvas_ids.view_id(view=ViewName.init_clone_view)
+        self.clone_repo = ids.view_id(view=ViewName.init_clone_view)
         self.clone_repo_q = f"#{self.clone_repo}"
-        self.new_repo = canvas_ids.view_id(view=ViewName.init_new_view)
+        self.new_repo = ids.view_id(view=ViewName.init_new_view)
         self.new_repo_q = f"#{self.new_repo}"
 
         # Config tab
-        self.cat_config = canvas_ids.view_id(view=ViewName.cat_config_view)
+        self.cat_config = ids.view_id(view=ViewName.cat_config_view)
         self.cat_config_q = f"#{self.cat_config}"
-        self.ignored = canvas_ids.view_id(view=ViewName.git_ignored_view)
+        self.ignored = ids.view_id(view=ViewName.git_ignored_view)
         self.ignored_q = f"#{self.ignored}"
 
         # Views or shared across canvases
-        self.pw_mgr_info = canvas_ids.view_id(view=ViewName.pw_mgr_info_view)
+        self.pw_mgr_info = ids.view_id(view=ViewName.pw_mgr_info_view)
         self.pw_mgr_info_q = f"#{self.pw_mgr_info}"
-        self.template_data = canvas_ids.view_id(
-            view=ViewName.template_data_view
-        )
+        self.template_data = ids.view_id(view=ViewName.template_data_view)
         self.template_data_q = f"#{self.template_data}"
