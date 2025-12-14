4c0116f9dde249cf5170f3ad328601b1fe5b9d2b
matmaer <22435003+matmaer@users.noreply.github.com>
2025-12-14 11:07:09 +0100

    Improve InitStaticText.

--- src/chezmoi_mousse/gui/operate.py
+++ src/chezmoi_mousse/gui/operate.py
@@ -73,8 +73,8 @@ class InitStaticText(StrEnum):
         "Let chezmoi guess the best ssh scp-style address to clone from."
     )
     https_url = "Enter a complete URL, e.g., [$text-primary]https://github.com/user/repo.git[/]."
-    pat_info = "If you have a PAT, make sure to include it in the URL, for example:\n[$text-primary]https://username:ghp_123456789abcdef@github.com/username/my-dotfiles.git[/]\nand delete the PAT after use."
-    ssh_select = "Enter an SSH SCP-style URL, e.g., git@github.com:user/repo.git. If your dotfiles repository is private, make sure you have your SSH key pair set up before using this option."
+    pat_info = "If you have a PAT, make sure to include it in the URL, for example: [$text-primary]https://username:ghp_123456789abcdef@github.com/username/my-dotfiles.git[/] and delete the PAT after use."
+    ssh_select = "Enter an SSH SCP-style URL, e.g., [$text-primary]git@github.com:user/repo.git[/]. If your dotfiles repository is private, make sure you have your SSH key pair set up before using this option."
 
 
 class InputURL(Input):
