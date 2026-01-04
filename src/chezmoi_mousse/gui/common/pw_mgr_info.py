bff267b807513d382aae84549b592cce9ba0571a
matmaer <22435003+matmaer@users.noreply.github.com>
2026-01-04 20:30:16 +0100

    Give PWCollapsible titles text-primary color.

--- src/chezmoi_mousse/gui/common/pw_mgr_info.py
+++ src/chezmoi_mousse/gui/common/pw_mgr_info.py
@@ -17,24 +17,6 @@ if TYPE_CHECKING:
 __all__ = ["PwMgrInfoView"]
 
 
-class DoctorChecks(StrEnum):
-    age_command = "age-command"
-    bitwarden_command = "bitwarden-command"
-    bitwarden_secrets_command = "bitwarden-secrets-command"
-    dashlane_command = "dashlane-command"
-    doppler_command = "doppler-command"
-    gopass_command = "gopass-command"
-    keepassxc_command = "keepassxc-command"
-    keeper_command = "keeper-command"
-    lastpass_command = "lastpass-command"
-    one_password_command = "1password-command"
-    pass_command = "pass-command"
-    passhole_command = "passhole-command"
-    pinentry_command = "pinentry-command"
-    rbw_command = "rbw-command"
-    vault_command = "vault-command"
-
-
 class InfoStrings(StrEnum):
     additional_info_label = "Additional Info"
     confusing = "Check your package manager which implementation is used as there are confusingly similar named packages."
@@ -56,91 +38,91 @@ class PwMgrData:
 
 class PwMgrInfo(Enum):
     age_command = PwMgrData(
-        doctor_check=DoctorChecks.age_command,
+        doctor_check="age-command",
         description="A simple, modern and secure file encryption tool.",
         link="https://github.com/FiloSottile/age",
         info=InfoStrings.fully_open_source,
     )
     bitwarden_command = PwMgrData(
-        doctor_check=DoctorChecks.bitwarden_command,
+        doctor_check="bitwarden-command",
         description="Bitwarden Password Manager",
         link="https://github.com/bitwarden",
         info=InfoStrings.source_available,
     )
     bitwarden_secrets_command = PwMgrData(
-        doctor_check=DoctorChecks.bitwarden_secrets_command,
+        doctor_check="bitwarden-secrets-command",
         description="Bitwarden Secrets Manager CLI for managing secrets securely.",
         link="https://github.com/bitwarden",
         info=InfoStrings.fully_open_source,
     )
     dashlane_command = PwMgrData(
-        doctor_check=DoctorChecks.dashlane_command,
+        doctor_check="dashlane-command",
         description="Simple and secure access to all your online accounts. At work, home, and everywhere in between.",
         link="https://github.com/dashlane",
         info=InfoStrings.not_open_source,
     )
     doppler_command = PwMgrData(
-        doctor_check=DoctorChecks.doppler_command,
+        doctor_check="doppler-command",
         description="Doppler is the multi-cloud SecretOps Platform developers and security teams trust to provide secrets management at enterprise scale.",
         link="https://github.com/dopplerhq",
         info=InfoStrings.not_open_source,
     )
     gopass_command = PwMgrData(
-        doctor_check=DoctorChecks.gopass_command,
+        doctor_check="gopass-command",
         description="The slightly more awesome standard unix password manager for teams.",
         link="https://github.com/gopasspw/gopass",
         info=InfoStrings.fully_open_source,
     )
     keeper_command = PwMgrData(
-        doctor_check=DoctorChecks.keeper_command,
+        doctor_check="keeper-command",
         description="An interface to Keeper Password Manager",
         link="https://github.com/Keeper-Security/Commander",
         info=InfoStrings.not_open_source,
     )
     keepassxc_command = PwMgrData(
-        doctor_check=DoctorChecks.keepassxc_command,
+        doctor_check="keepassxc-command",
         description="Cross-platform community-driven port of Keepass password manager.",
         link="https://keepassxc.org/",
         info=InfoStrings.fully_open_source,
     )
     lastpass_command = PwMgrData(
-        doctor_check=DoctorChecks.lastpass_command,
+        doctor_check="lastpass-command",
         description="Old LastPass CLI for accessing your LastPass vault.",
         link="https://https://github.com/lastpass",
         info=InfoStrings.not_open_source,
     )
     one_password_command = PwMgrData(
-        doctor_check=DoctorChecks.one_password_command,
+        doctor_check="one-password-command",
         description="Secure all sign-ins to every application from any device.",
         link="https://github.com/1Password/for-open-source",
         info=InfoStrings.not_open_source,
     )
     pass_command = PwMgrData(
-        doctor_check=DoctorChecks.pass_command,
+        doctor_check="pass-command",
         description="Stores, retrieves, generates, and synchronizes passwords securely.",
         link="https://www.passwordstore.org/",
         info=InfoStrings.confusing,
     )
     passhole_command = PwMgrData(
-        doctor_check=DoctorChecks.passhole_command,
+        doctor_check="passhole-command",
         description="A secure hole for your passwords (KeePass CLI).",
         link="https://github.com/Evidlo/passhole",
         info=InfoStrings.not_open_source,
     )
     pinentry_command = PwMgrData(
-        doctor_check=DoctorChecks.pinentry_command,
+        doctor_check="pinentry-command",
         description="Collection of simple PIN or passphrase entry dialogs which utilize the Assuan protocol.",
         link="https://gnupg.org/related_software/pinentry/",
         info=InfoStrings.fully_open_source,
     )
     rbw_command = PwMgrData(
-        doctor_check=DoctorChecks.rbw_command,
+        doctor_check="rbw-command",
         description="Unofficial Bitwarden.",
         link="https://git.tozt.net/rbw",
         info=InfoStrings.not_documented,
     )
     vault_command = PwMgrData(
-        doctor_check=DoctorChecks.vault_command,
+        doctor_check="vault-command",
         description="A tool for managing secrets.",
         link="https://vaultproject.io/",
         info=InfoStrings.not_documented,
@@ -167,9 +149,6 @@ class PwCollapsible(CustomCollapsible, AppType):
         self.stripped_link = self.pw_mgr_data.link.replace(
             "https://", ""
         ).replace("www.", "")
-        self.collapsible_title = (
-            f"Doctor check: {self.pw_mgr_data.doctor_check}"
-        )
 
         super().__init__(
             VerticalGroup(
@@ -189,7 +168,10 @@ class PwCollapsible(CustomCollapsible, AppType):
                 Static(self.pw_mgr_data.info, markup=False),
                 classes=Tcss.pw_mgr_group,
             ),
-            title=self.collapsible_title,
+            title=(
+                f"[$text-primary]Doctor check: "
+                f"{self.pw_mgr_data.doctor_check}[/]"
+            ),
         )
 
 
@@ -219,4 +201,4 @@ class PwMgrInfoView(Vertical):
             pw_collapsible = PwCollapsible(pw_mgr_data=item)
             self.mount(pw_collapsible)
 
-        self.mount(Static(InfoStrings.info_warning))
+        self.mount(Static(f"\n{InfoStrings.info_warning}"))
