c17c44d2bc723a5e315739c5bfd0944c0d9e84c5
matmaer <22435003+matmaer@users.noreply.github.com>
2026-01-24 15:37:56 +0100

    Introduce widget factory.

--- src/chezmoi_mousse/widget_factory.py
+++ src/chezmoi_mousse/widget_factory.py
@@ -6,102 +6,150 @@
 from dataclasses import dataclass
 from pathlib import Path
 
-from rich.highlighter import ReprHighlighter
-from rich.text import Text
-from textual.widgets import DataTable, Label, Static, TextArea
-from textual.widgets.text_area import BUILTIN_LANGUAGES
+# from rich.highlighter import ReprHighlighter
+# from rich.text import Text
+from textual.widgets import DataTable, Static, TextArea
 
-from ._chezmoi_command import ReadCmd
-from ._str_enum_names import PathKind, Tcss
+from ._chezmoi_command import CommandResult, ReadCmd
+from ._str_enum_names import TabName
 from ._str_enums import StatusCode
 from ._type_checking import AppType
 
-__all__ = ["GitLogTable"]
+# from textual.widgets.text_area import BUILTIN_LANGUAGES
+
+
+__all__ = ["DirNodeDict"]
 
 
 @dataclass(slots=True)
-class PathWidgets:
-    label_str: str
+class PathData:
     contents: list[Static] | TextArea
-    diff_apply: list[Static]
-    diff_reverse: list[Static]
+    diff: list[Static]
     git_log: DataTable[str]
+    label: str
+    status: StatusCode
 
 
-class PathNode(AppType):
+type FileNodeDict = dict[Path, PathData]
+
+
+@dataclass(slots=True)
+class DirData:
+    files: FileNodeDict
+    widgets: PathData
+
+
+class DirNodeDict(AppType):
+
+    dest_dir: Path
 
     def __init__(
-        self, path: Path, path_kind: PathKind, status_code: StatusCode
+        self,
+        git_log_cmd_result: CommandResult,
+        managed_dirs_cmd_result: CommandResult,
+        managed_files_cmd_result: CommandResult,
+        status_dirs_cmd_result: CommandResult,
+        status_files_cmd_result: CommandResult,
+        tab_name: TabName,
     ) -> None:
-        self.status_code = status_code
-        self.path = path
-        self.path_kind = path_kind
-        self.path_widgets: PathWidgets = self.create_widgets()
-
-    def create_widgets(self):
-        if self.path_kind == PathKind.FILE:
-            path_contents = self.create_file_content_widgets()
-        else:
-            path_contents = self.create_dir_content_widgets()
-
-        return PathWidgets(
-            label_str=self.path.name,  # to be constructed based status_code and exists
-            contents=path_contents,
-            diff_apply=self.create_diff_statics(reverse=False),
-            diff_reverse=self.create_diff_statics(reverse=True),
-            git_log=GitLogTable(self.path),
-        )
-
-    def create_diff_statics(self, reverse: bool) -> list[Static]:
-        # green: "+", "new"
-        # red: "-", "old", "deleted"
-        # orange: "changed"
-        # dimmed: " ", "index"
-        static_list: list[Static] = []
-        diff_result = (
-            (self.app.cmd.read(ReadCmd.diff, path_arg=self.path))
-            if reverse is False
-            else (self.app.cmd.read(ReadCmd.diff_reverse, path_arg=self.path))
-        )
-        for line in diff_result.completed_process.stdout.splitlines():
-            if line.startswith(("+", "new")):
-                static_list.append(Static(line, classes=Tcss.added))
-            elif line.startswith(("-", "old", "deleted")):
-                static_list.append(Static(line, classes=Tcss.removed))
-            else:
-                static_list.append(Static(line, classes=Tcss.unchanged))
-        return static_list
-
-    # textual.textualize.io/widgets/text_area/#textual.widgets.text_area.BUILTIN_LANGUAGES
-    def create_file_content_widgets(self) -> TextArea | list[Static]:
-        BUILTIN_MAP = {lang: lang for lang in BUILTIN_LANGUAGES}
-        LANGUAGE_MAP = BUILTIN_MAP | {
-            ".cfg": BUILTIN_MAP["toml"],
-            ".ini": BUILTIN_MAP["toml"],
-            ".sh": BUILTIN_MAP["bash"],
-            ".yml": BUILTIN_MAP["yaml"],
-            ".zsh": BUILTIN_MAP["bash"],
+        self.git_log = git_log_cmd_result
+        self.managed_dirs = managed_dirs_cmd_result
+        self.managed_files = managed_files_cmd_result
+        self.status_dirs = status_dirs_cmd_result
+        self.status_files = status_files_cmd_result
+        self.tab_name = tab_name
+        # dir_path: Path, dir_files: list[Path], status_pair: tuple[str, str]
+        # self.path_widgets: PathData = self.create_widgets()
+        self.node_colors: dict[str, str] = {
+            StatusCode.Added: self.app.theme_variables["text-success"],
+            StatusCode.Deleted: self.app.theme_variables["text-error"],
+            StatusCode.Modified: self.app.theme_variables["text-warning"],
+            StatusCode.No_Change: self.app.theme_variables["warning-darken-2"],
+            StatusCode.X: self.app.theme_variables["foreground-darken-3"],
         }
-        text_content = self.app.cmd.read(
-            ReadCmd.cat, path_arg=self.path
-        ).completed_process.stdout
-        language = LANGUAGE_MAP.get(self.path.suffix.lower())
-        if language is not None:
-            return TextArea(text=text_content, language=language)
-        else:
-            rich_highlighter = ReprHighlighter()
-            static_list: list[Static] = []
-            for line in text_content.splitlines():
-                text_obj = Text(line)
-                rich_highlighter.highlight(text_obj)
-                static_list.append(Static(text_obj))
-            return static_list
+        self.dir_with_status_children_color = self.app.theme_variables["text-secondary"]
 
-    def create_dir_content_widgets(self) -> list[Label | Static]:
-        return [
-            Label("no label implemented"),
-            Static(f"no static implemented for {self.path}"),
-        ]
+    # def create_dir_widgets(self):
+
+    #     path_contents = self.create_dir_content_widgets()
+    #     label_str = (self.path.name,)  # to be constructed based status_code and exists
+    #     contents = (path_contents,)
+    #     diff_apply = (self.create_diff_statics(reverse=False),)
+    #     diff_reverse = (self.create_diff_statics(reverse=True),)
+    #     git_log = (GitLogTable(self.path),)
+    #     status_apply = (StatusCode(self.status_pair[0]),)
+    #     status_other = (StatusCode(self.status_pair[1]),)
+
+    # def create_file_widgets(self):
+    #     path_contents = self.create_dir_content_widgets()
+    #     label_str = (self.path.name,)  # to be constructed based status_code and exists
+    #     contents = (path_contents,)
+    #     diff_apply = (self.create_diff_statics(reverse=False),)
+    #     diff_reverse = (self.create_diff_statics(reverse=True),)
+    #     git_log = (GitLogTable(self.path),)
+    #     status_apply = (StatusCode(self.status_pair[0]),)
+    #     status_other = (StatusCode(self.status_pair[1]),)
+
+    # def create_label_string(self) -> None:
+    #     if self.path.exists():
+    #         italic = " italic" if not self.path.exists() else ""
+    #         color = self.node_colors[tree_node.data.status]
+    #         node_label = f"[{color}" f"{italic}]{tree_node.data.path.name}[/]"
+    #         if tree_node.data.path_kind == PathKind.FILE:
+    #             tree_node.add_leaf(label=node_label, data=tree_node.data)
+    #         else:
+    #             tree_node.add(label=node_label, data=tree_node.data)
+
+    # def create_diff_statics(self, reverse: bool) -> list[Static]:
+    #     # green: "+", "new"
+    #     # red: "-", "old", "deleted"
+    #     # orange: "changed"
+    #     # dimmed: " ", "index"
+    #     static_list: list[Static] = []
+    #     diff_result = (
+    #         (self.app.cmd.read(ReadCmd.diff, path_arg=self.path))
+    #         if reverse is False
+    #         else (self.app.cmd.read(ReadCmd.diff_reverse, path_arg=self.path))
+    #     )
+    #     for line in diff_result.completed_process.stdout.splitlines():
+    #         if line.startswith(("+", "new")):
+    #             static_list.append(Static(line, classes=Tcss.added))
+    #         elif line.startswith(("-", "old", "deleted")):
+    #             static_list.append(Static(line, classes=Tcss.removed))
+    #         else:
+    #             static_list.append(Static(line, classes=Tcss.unchanged))
+    #     return static_list
+
+    # # textual.textualize.io/widgets/text_area/#textual.widgets.text_area.BUILTIN_LANGUAGES
+    # def create_file_content_widgets(self) -> TextArea | list[Static]:
+    #     BUILTIN_MAP = {lang: lang for lang in BUILTIN_LANGUAGES}
+    #     LANGUAGE_MAP = BUILTIN_MAP | {
+    #         ".cfg": BUILTIN_MAP["toml"],
+    #         ".ini": BUILTIN_MAP["toml"],
+    #         ".sh": BUILTIN_MAP["bash"],
+    #         ".yml": BUILTIN_MAP["yaml"],
+    #         ".zsh": BUILTIN_MAP["bash"],
+    #     }
+    #     text_content = self.app.cmd.read(
+    #         ReadCmd.cat, path_arg=self.path
+    #     ).completed_process.stdout
+    #     language = LANGUAGE_MAP.get(self.path.suffix.lower())
+    #     if language is not None:
+    #         return TextArea(text=text_content, language=language)
+    #     else:
+    #         rich_highlighter = ReprHighlighter()
+    #         static_list: list[Static] = []
+    #         for line in text_content.splitlines():
+    #             text_obj = Text(line)
+    #             rich_highlighter.highlight(text_obj)
+    #             static_list.append(Static(text_obj))
+    #         return static_list
+
+    # def create_dir_content_widgets(self) -> list[Label | Static]:
+    #     return [
+    #         Label("no label implemented"),
+    #         Static(f"no static implemented for {self.path}"),
+    #     ]
 
 
 class GitLogTable(DataTable[str], AppType):
