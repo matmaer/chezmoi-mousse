fc430d753b4303d507dc91d86693ea106398cdce
matmaer <22435003+matmaer@users.noreply.github.com>
2026-02-08 03:33:07 +0100

    Create ReactiveDataclass class.

--- src/chezmoi_mousse/_type_checking.py
+++ src/chezmoi_mousse/_type_checking.py
@@ -17,6 +17,24 @@ class AppType:
     app: ChezmoiGUI
 
 
+class ReactiveDataclass:
+    # Base class for dataclasses to trigger logic on field updates.
+    # The calls to super().__setattr__() ensure that Python internals work correctly.
+
+    def __setattr__(self, name: str, value: Any) -> None:
+        if hasattr(self, name):
+            old_value = getattr(self, name)
+            super().__setattr__(name, value)
+            if old_value != value:
+                self._on_field_change(name, old_value, value)
+        else:
+            super().__setattr__(name, value)
+
+    def _on_field_change(self, name: str, old_value: Any, new_value: Any) -> None:
+        # Override in subclasses to define update logic
+        pass
+
+
 @dataclass(slots=True)
 class CmdResults:
     cat_config: CommandResult | None = None
@@ -30,6 +48,7 @@ class CmdResults:
     status_files: CommandResult | None = None
     template_data: CommandResult | None = None
     verify: CommandResult | None = None
+    # managed_dirs: list[Path] = field(default_factory=list[Path])
 
     @property
     def executed_commands(self) -> list[CommandResult]:
@@ -39,6 +58,18 @@ class CmdResults:
             for field in fields(self)
             if getattr(self, field.name) is not None
         ]
+
+    @property
+    def managed_dir_paths(self) -> list[Path]:
+        if self.managed_dirs is None:
+            return []
+        return [Path(line) for line in self.managed_dirs.std_out.splitlines()]
+
+    @property
+    def managed_file_paths(self) -> list[Path]:
+        if self.managed_files is None:
+            return []
+        return [Path(line) for line in self.managed_files.std_out.splitlines()]
 
 
 @dataclass(slots=True)
