13af547c57fad977b1989594e1849dcfcba9aeed
matmaer <22435003+matmaer@users.noreply.github.com>
2026-02-22 12:26:56 +0100

    Correct creating diffs in test paths.

--- src/chezmoi_mousse/_test_paths.py
+++ src/chezmoi_mousse/_test_paths.py
@@ -248,7 +248,11 @@ class TestPaths:
 
         # Modify TOML files
         for file in self.file_paths:
-            if file.exists() and file.name not in self.unchanged_file_names:
+            if (
+                file.exists()
+                and file.name not in self.unchanged_file_names
+                and file.suffix == ".toml"
+            ):
                 modified_content = (
                     file.read_text(encoding="utf-8")
                     .replace("title", "new_title")
