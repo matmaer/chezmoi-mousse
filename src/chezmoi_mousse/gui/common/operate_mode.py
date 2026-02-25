4150c6dac0dd8d80a8fb7388dac140b7fa998659
matmaer <22435003+matmaer@users.noreply.github.com>
2026-02-25 19:04:44 +0100

    Update read outputs after a write command.

--- src/chezmoi_mousse/gui/common/operate_mode.py
+++ src/chezmoi_mousse/gui/common/operate_mode.py
@@ -11,10 +11,12 @@ from textual.widgets import Label, LoadingIndicator, Static
 
 from chezmoi_mousse import (
     CMD,
+    CMD_RESULTS,
     AppType,
     CommandResult,
     OpBtnEnum,
     OperateString,
+    ReadCmd,
     TabName,
     Tcss,
 )
@@ -108,6 +110,18 @@ class OperateMode(Vertical, AppType):
     async def _run_perform_command(self, btn_enum: OpBtnEnum) -> CommandResult:
         return CMD.perform(btn_enum.write_cmd, path_arg=self.path_arg)
 
+    @work
+    async def _update_read_outputs(self) -> None:
+        for read_cmd in (
+            ReadCmd.managed_dirs,
+            ReadCmd.managed_files,
+            ReadCmd.status_dirs,
+            ReadCmd.status_files,
+        ):
+            cmd_result = CMD.read(read_cmd)
+            setattr(CMD_RESULTS, f"{read_cmd.name}", cmd_result)
+        self.app.cmd_results.new_results = CMD_RESULTS
+
     @work(exit_on_error=False)
     async def run_command(self, btn_enum: OpBtnEnum) -> None:
         pretty_cmd = btn_enum.write_cmd.pretty_cmd
@@ -124,6 +138,8 @@ class OperateMode(Vertical, AppType):
         if cmd_result is None:
             raise RuntimeError(f"CommandResult is None for {btn_enum.write_cmd.name}")
         self._update_post_run_info(cmd_result)
+        read_cmd_worker = self._update_read_outputs()
+        await read_cmd_worker.wait()
         self.op_results_container.mount(cmd_result.pretty_collapsible)
         elapsed = time.monotonic() - start_time
         if elapsed < 1.0:
