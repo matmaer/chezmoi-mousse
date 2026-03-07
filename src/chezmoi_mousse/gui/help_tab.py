6f2c956983395a6b561f900ff1ed1fbc72173c94
matmaer <22435003+matmaer@users.noreply.github.com>
2026-03-07 03:26:47 +0100

    Implement the help tab later on.

--- src/chezmoi_mousse/gui/help_tab.py
+++ src/chezmoi_mousse/gui/help_tab.py
@@ -53,12 +53,12 @@ class HelpStrings(StrEnum):
     unwanted_filter = f"{SwitchEnum.unwanted.label} Filter"
     # Apply tab
     apply_tab_help = "Apply Tab Help"
-    apply_dir_button = f"{OpBtnLabel.apply_review} Button"
-    apply_file_button = f"{OpBtnLabel.apply_run} Button"
+    apply_review_button = f"{OpBtnLabel.apply_review} Button"
+    apply_run_button = f"{OpBtnLabel.apply_run} Button"
     # Re-Add tab
     re_add_tab_help = "Re-Add Tab Help"
-    re_add_dir_button = f"{OpBtnLabel.re_add_review} Button"
-    re_add_file_button = f"{OpBtnLabel.re_add_run} Button"
+    re_add_review_button = f"{OpBtnLabel.re_add_review} Button"
+    re_add_run_button = f"{OpBtnLabel.re_add_run} Button"
     # Common filters for Apply and Re-Add tabs
     expand_all_filter = f"{SwitchEnum.expand_all.label} Filter"
     unchanged_filter = f"{SwitchEnum.unchanged.label} Filter"
@@ -68,14 +68,12 @@ class SharedBtnHelp(VerticalGroup):
 
     def compose(self) -> ComposeResult:
         yield FlatLink(LinkBtn.chezmoi_forget)
-        yield Label(HelpStrings.available_buttons, classes=Tcss.sub_section_label)
         yield FlatLink(LinkBtn.chezmoi_destroy)
-        yield Label(HelpStrings.available_buttons, classes=Tcss.sub_section_label)
 
 
 class SharedFiltersHelp(VerticalGroup):
     def compose(self) -> ComposeResult:
-        yield Label(HelpStrings.available_filters, classes=Tcss.flat_section_label)
+        yield Label(HelpStrings.available_filters, classes=Tcss.main_section_label)
         yield Label(HelpStrings.unchanged_filter, classes=Tcss.sub_section_label)
         yield Static(SwitchEnum.unchanged.enabled_tooltip)
         yield Label(HelpStrings.expand_all_filter, classes=Tcss.sub_section_label)
@@ -90,10 +88,10 @@ class ApplyTabHelp(Vertical):
         yield Label(HelpStrings.apply_tab_help, classes=Tcss.main_section_label)
         with ScrollableContainer():
             yield SharedFiltersHelp()
+            yield Label(HelpStrings.available_buttons, classes=Tcss.main_section_label)
             yield FlatLink(LinkBtn.chezmoi_apply)
-            yield Label(HelpStrings.available_buttons, classes=Tcss.sub_section_label)
-            yield Static(HelpStrings.apply_file_button)
-            yield Static(HelpStrings.apply_dir_button)
+            yield Static(HelpStrings.apply_review_button)
+            yield Static(HelpStrings.apply_run_button)
             yield SharedBtnHelp()
 
 
@@ -106,10 +104,10 @@ class ReAddTabHelp(Vertical):
         yield Label(HelpStrings.re_add_tab_help, classes=Tcss.main_section_label)
         with ScrollableContainer():
             yield SharedFiltersHelp()
+            yield Label(HelpStrings.available_buttons, classes=Tcss.main_section_label)
+            yield Static(HelpStrings.re_add_review_button)
+            yield Static(HelpStrings.re_add_run_button)
             yield FlatLink(LinkBtn.chezmoi_re_add)
-            yield Label(HelpStrings.available_buttons, classes=Tcss.sub_section_label)
-            yield Static(HelpStrings.re_add_file_button)
-            yield Static(HelpStrings.re_add_dir_button)
             yield SharedBtnHelp()
 
 
@@ -122,15 +120,15 @@ class AddTabHelp(Vertical):
         yield Label(HelpStrings.add_tab_help, classes=Tcss.main_section_label)
 
         with ScrollableContainer():
-            yield Label(HelpStrings.available_filters, classes=Tcss.flat_section_label)
+            yield Label(HelpStrings.available_filters, classes=Tcss.main_section_label)
             yield Label(
                 HelpStrings.unmanaged_dirs_filter, classes=Tcss.sub_section_label
             )
             yield Static(SwitchEnum.unmanaged_dirs.enabled_tooltip)
             yield Label(HelpStrings.unwanted_filter, classes=Tcss.sub_section_label)
             yield Static(SwitchEnum.unwanted.enabled_tooltip)
-            yield FlatLink(LinkBtn.chezmoi_add)
             yield Label(HelpStrings.available_buttons, classes=Tcss.sub_section_label)
+            yield FlatLink(LinkBtn.chezmoi_add)
 
 
 class ChezmoiDiagram(Vertical):
