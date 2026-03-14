f0c0c0d65df0d70be1babff2fdac00014bbb0062
matmaer <22435003+matmaer@users.noreply.github.com>
2026-03-14 19:37:27 +0100

    Be more explicit about types.

--- src/chezmoi_mousse/_app_ids.py
+++ src/chezmoi_mousse/_app_ids.py
@@ -140,149 +140,161 @@ class AppIds:
                 f"Unexpected canvas_name {self.canvas_name} for tab_operation_btn_q"
             )
 
-    @property
-    def cancel_btn_q(self) -> set[str]:
-        return {self.op_btn.cancel_q, self.op_btn.cancel_q}
-
 
 class ContainerIds:
     def __init__(self, ids: AppIds):
-        self.contents = ids.container_id(name=ContainerName.contents)
-        self.contents_q = f"#{self.contents}"
-        self.command_output = ids.container_id(name=ContainerName.command_output)
-        self.command_output_q = f"#{self.command_output}"
-        self.diff = ids.container_id(name=ContainerName.diff)
-        self.diff_q = f"#{self.diff}"
-        self.dir_contents = ids.container_id(name=ContainerName.dir_contents)
-        self.dir_contents_q = f"#{self.dir_contents}"
-        self.doctor = ids.container_id(name=ContainerName.doctor)
-        self.doctor_q = f"#{self.doctor}"
-        self.file_contents = ids.container_id(name=ContainerName.file_contents)
-        self.file_contents_q = f"#{self.file_contents}"
-        self.git_log = ids.container_id(name=ContainerName.git_log)
-        self.git_log_q = f"#{self.git_log}"
-        self.left_side = ids.container_id(name=ContainerName.left_side)
-        self.left_side_q = f"#{self.left_side}"
-        self.operate_buttons = ids.container_id(name=ContainerName.operate_buttons)
-        self.operate_buttons_q = f"#{self.operate_buttons}"
-        self.op_mode = ids.container_id(name=ContainerName.op_mode)
-        self.op_mode_q = f"#{self.op_mode}"
-        self.op_cmd_results = ids.container_id(name=ContainerName.op_cmd_results)
-        self.op_cmd_results_q = f"#{self.op_cmd_results}"
-        self.repo_input = ids.container_id(name=ContainerName.repo_input)
-        self.repo_input_q = f"#{self.repo_input}"
-        self.right_side = ids.container_id(name=ContainerName.right_side)
-        self.right_side_q = f"#{self.right_side}"
+        self.contents: str = ids.container_id(name=ContainerName.contents)
+        self.contents_q: str = f"#{self.contents}"
+        self.command_output: str = ids.container_id(name=ContainerName.command_output)
+        self.command_output_q: str = f"#{self.command_output}"
+        self.diff: str = ids.container_id(name=ContainerName.diff)
+        self.diff_q: str = f"#{self.diff}"
+        self.dir_contents: str = ids.container_id(name=ContainerName.dir_contents)
+        self.dir_contents_q: str = f"#{self.dir_contents}"
+        self.doctor: str = ids.container_id(name=ContainerName.doctor)
+        self.doctor_q: str = f"#{self.doctor}"
+        self.file_contents: str = ids.container_id(name=ContainerName.file_contents)
+        self.file_contents_q: str = f"#{self.file_contents}"
+        self.git_log: str = ids.container_id(name=ContainerName.git_log)
+        self.git_log_q: str = f"#{self.git_log}"
+        self.left_side: str = ids.container_id(name=ContainerName.left_side)
+        self.left_side_q: str = f"#{self.left_side}"
+        self.operate_buttons: str = ids.container_id(name=ContainerName.operate_buttons)
+        self.operate_buttons_q: str = f"#{self.operate_buttons}"
+        self.op_mode: str = ids.container_id(name=ContainerName.op_mode)
+        self.op_mode_q: str = f"#{self.op_mode}"
+        self.op_cmd_results: str = ids.container_id(name=ContainerName.op_cmd_results)
+        self.op_cmd_results_q: str = f"#{self.op_cmd_results}"
+        self.repo_input: str = ids.container_id(name=ContainerName.repo_input)
+        self.repo_input_q: str = f"#{self.repo_input}"
+        self.right_side: str = ids.container_id(name=ContainerName.right_side)
+        self.right_side_q: str = f"#{self.right_side}"
 
 
 class LabelIds:
     """Label widgets their id's to target for show/hide or update the text."""
 
     def __init__(self, ids: AppIds):
-        self.cat_config_output = ids.label_id(label=LabelName.cat_config_output)
-        self.cat_config_output_q = f"#{self.cat_config_output}"
-        self.loading = ids.label_id(label=LabelName.loading)
-        self.loading_q = f"#{self.loading}"
+        self.cat_config_output: str = ids.label_id(label=LabelName.cat_config_output)
+        self.cat_config_output_q: str = f"#{self.cat_config_output}"
+        self.loading: str = ids.label_id(label=LabelName.loading)
+        self.loading_q: str = f"#{self.loading}"
 
 
 class LoggerIds:
     """RichLog widgets their id's."""
 
     def __init__(self, ids: AppIds):
-        self.app = ids.view_id(view=LogName.app_logger)
-        self.app_q = f"#{self.app}"
-        self.cmd = ids.view_id(view=LogName.cmd_logger)
-        self.cmd_q = f"#{self.cmd}"
-        self.debug = ids.view_id(view=LogName.debug_logger)
-        self.debug_q = f"#{self.debug}"
-        self.dom_nodes = ids.view_id(view=LogName.dom_node_logger)
-        self.dom_nodes_q = f"#{self.dom_nodes}"
-        self.memory = ids.view_id(view=LogName.memory_usage_logger)
-        self.memory_q = f"#{self.memory}"
+        self.app: str = ids.view_id(view=LogName.app_logger)
+        self.app_q: str = f"#{self.app}"
+        self.cmd: str = ids.view_id(view=LogName.cmd_logger)
+        self.cmd_q: str = f"#{self.cmd}"
+        self.debug: str = ids.view_id(view=LogName.debug_logger)
+        self.debug_q: str = f"#{self.debug}"
+        self.dom_nodes: str = ids.view_id(view=LogName.dom_node_logger)
+        self.dom_nodes_q: str = f"#{self.dom_nodes}"
+        self.memory: str = ids.view_id(view=LogName.memory_usage_logger)
+        self.memory_q: str = f"#{self.memory}"
 
 
 class OperateButtonIds:
     def __init__(self, ids: AppIds):
-        self.add_review = ids.operate_button_id(operation=OpBtnLabel.add_review)
-        self.add_review_q = f"#{self.add_review}"
-        self.add_run = ids.operate_button_id(operation=OpBtnLabel.add_run)
-        self.add_run_q = f"#{self.add_run}"
+        self.add_review: str = ids.operate_button_id(operation=OpBtnLabel.add_review)
+        self.add_review_q: str = f"#{self.add_review}"
+        self.add_run: str = ids.operate_button_id(operation=OpBtnLabel.add_run)
+        self.add_run_q: str = f"#{self.add_run}"
 
-        self.apply_review = ids.operate_button_id(operation=OpBtnLabel.apply_review)
-        self.apply_review_q = f"#{self.apply_review}"
-        self.apply_run = ids.operate_button_id(operation=OpBtnLabel.apply_run)
-        self.apply_run_q = f"#{self.apply_run}"
+        self.apply_review: str = ids.operate_button_id(
+            operation=OpBtnLabel.apply_review
+        )
+        self.apply_review_q: str = f"#{self.apply_review}"
+        self.apply_run: str = ids.operate_button_id(operation=OpBtnLabel.apply_run)
+        self.apply_run_q: str = f"#{self.apply_run}"
 
-        self.destroy_review = ids.operate_button_id(operation=OpBtnLabel.destroy_review)
-        self.destroy_review_q = f"#{self.destroy_review}"
-        self.destroy_run = ids.operate_button_id(operation=OpBtnLabel.destroy_run)
-        self.destroy_run_q = f"#{self.destroy_run}"
+        self.destroy_review: str = ids.operate_button_id(
+            operation=OpBtnLabel.destroy_review
+        )
+        self.destroy_review_q: str = f"#{self.destroy_review}"
+        self.destroy_run: str = ids.operate_button_id(operation=OpBtnLabel.destroy_run)
+        self.destroy_run_q: str = f"#{self.destroy_run}"
 
-        self.forget_review = ids.operate_button_id(operation=OpBtnLabel.forget_review)
-        self.forget_review_q = f"#{self.forget_review}"
-        self.forget_run = ids.operate_button_id(operation=OpBtnLabel.forget_run)
-        self.forget_run_q = f"#{self.forget_run}"
+        self.forget_review: str = ids.operate_button_id(
+            operation=OpBtnLabel.forget_review
+        )
+        self.forget_review_q: str = f"#{self.forget_review}"
+        self.forget_run: str = ids.operate_button_id(operation=OpBtnLabel.forget_run)
+        self.forget_run_q: str = f"#{self.forget_run}"
 
-        self.init_review = ids.operate_button_id(operation=OpBtnLabel.init_review)
-        self.init_review_q = f"#{self.init_review}"
-        self.init_run = ids.operate_button_id(operation=OpBtnLabel.init_run)
-        self.init_run_q = f"#{self.init_run}"
+        self.init_review: str = ids.operate_button_id(operation=OpBtnLabel.init_review)
+        self.init_review_q: str = f"#{self.init_review}"
+        self.init_run: str = ids.operate_button_id(operation=OpBtnLabel.init_run)
+        self.init_run_q: str = f"#{self.init_run}"
 
-        self.re_add_review = ids.operate_button_id(operation=OpBtnLabel.re_add_review)
-        self.re_add_review_q = f"#{self.re_add_review}"
-        self.re_add_run = ids.operate_button_id(operation=OpBtnLabel.re_add_run)
-        self.re_add_run_q = f"#{self.re_add_run}"
+        self.re_add_review: str = ids.operate_button_id(
+            operation=OpBtnLabel.re_add_review
+        )
+        self.re_add_review_q: str = f"#{self.re_add_review}"
+        self.re_add_run: str = ids.operate_button_id(operation=OpBtnLabel.re_add_run)
+        self.re_add_run_q: str = f"#{self.re_add_run}"
 
-        self.cancel = ids.operate_button_id(operation=OpBtnLabel.cancel)
-        self.cancel_q = f"#{self.cancel}"
-        self.exit_app = ids.operate_button_id(operation=OpBtnLabel.exit_app)
-        self.exit_app_q = f"#{self.exit_app}"
-        self.refresh_tree = ids.operate_button_id(operation=OpBtnLabel.refresh_tree)
-        self.refresh_tree_q = f"#{self.refresh_tree}"
-        self.reload = ids.operate_button_id(operation=OpBtnLabel.reload)
-        self.reload_q = f"#{self.reload}"
+        self.cancel: str = ids.operate_button_id(operation=OpBtnLabel.cancel)
+        self.cancel_q: str = f"#{self.cancel}"
+        self.exit_app: str = ids.operate_button_id(operation=OpBtnLabel.exit_app)
+        self.exit_app_q: str = f"#{self.exit_app}"
+        self.refresh_tree: str = ids.operate_button_id(
+            operation=OpBtnLabel.refresh_tree
+        )
+        self.refresh_tree_q: str = f"#{self.refresh_tree}"
+        self.reload: str = ids.operate_button_id(operation=OpBtnLabel.reload)
+        self.reload_q: str = f"#{self.reload}"
 
         # for test_paths only
-        self.create_paths = ids.operate_button_id(operation=OpBtnLabel.create_paths)
-        self.create_paths_q = f"#{self.create_paths}"
-        self.remove_paths = ids.operate_button_id(operation=OpBtnLabel.remove_paths)
-        self.remove_paths_q = f"#{self.remove_paths}"
-        self.create_diffs = ids.operate_button_id(operation=OpBtnLabel.create_diffs)
-        self.create_diffs_q = f"#{self.create_diffs}"
-        self.log_memory = ids.operate_button_id(operation=OpBtnLabel.log_memory)
-        self.log_memory_q = f"#{self.log_memory}"
+        self.create_paths: str = ids.operate_button_id(
+            operation=OpBtnLabel.create_paths
+        )
+        self.create_paths_q: str = f"#{self.create_paths}"
+        self.remove_paths: str = ids.operate_button_id(
+            operation=OpBtnLabel.remove_paths
+        )
+        self.remove_paths_q: str = f"#{self.remove_paths}"
+        self.create_diffs: str = ids.operate_button_id(
+            operation=OpBtnLabel.create_diffs
+        )
+        self.create_diffs_q: str = f"#{self.create_diffs}"
+        self.log_memory: str = ids.operate_button_id(operation=OpBtnLabel.log_memory)
+        self.log_memory_q: str = f"#{self.log_memory}"
 
 
 class StaticIds:
     def __init__(self, ids: AppIds):
-        self.debug_test_paths = ids.static_id(static=StaticName.debug_test_paths)
-        self.debug_test_paths_q = f"#{self.debug_test_paths}"
-        self.init_info = ids.static_id(static=StaticName.init_info)
-        self.init_info_q = f"#{self.init_info}"
-        self.operate_info = ids.static_id(static=StaticName.operate_info)
-        self.operate_info_q = f"#{self.operate_info}"
+        self.debug_test_paths: str = ids.static_id(static=StaticName.debug_test_paths)
+        self.debug_test_paths_q: str = f"#{self.debug_test_paths}"
+        self.init_info: str = ids.static_id(static=StaticName.init_info)
+        self.init_info_q: str = f"#{self.init_info}"
+        self.operate_info: str = ids.static_id(static=StaticName.operate_info)
+        self.operate_info_q: str = f"#{self.operate_info}"
 
 
 class TreeIds:
     """Tree widget their id's."""
 
     def __init__(self, ids: AppIds):
-        self.list = ids.tree_id(tree=TreeName.list_tree)
-        self.list_q = f"#{self.list}"
-        self.managed = ids.tree_id(tree=TreeName.managed_tree)
-        self.managed_q = f"#{self.managed}"
+        self.list: str = ids.tree_id(tree=TreeName.list_tree)
+        self.list_q: str = f"#{self.list}"
+        self.managed: str = ids.tree_id(tree=TreeName.managed_tree)
+        self.managed_q: str = f"#{self.managed}"
 
 
 class SwitchIds:
     def __init__(self, ids: AppIds):
-        self.expand_all = ids.switch_id(switch=SwitchEnum.expand_all)
-        self.expand_all_q = f"#{self.expand_all}"
-        self.unchanged = ids.switch_id(switch=SwitchEnum.unchanged)
-        self.unchanged_q = f"#{self.unchanged}"
-        self.unmanaged_dirs = ids.switch_id(switch=SwitchEnum.unmanaged_dirs)
-        self.unmanaged_dirs_q = f"#{self.unmanaged_dirs}"
-        self.unwanted = ids.switch_id(switch=SwitchEnum.unwanted)
-        self.unwanted_q = f"#{self.unwanted}"
+        self.expand_all: str = ids.switch_id(switch=SwitchEnum.expand_all)
+        self.expand_all_q: str = f"#{self.expand_all}"
+        self.unchanged: str = ids.switch_id(switch=SwitchEnum.unchanged)
+        self.unchanged_q: str = f"#{self.unchanged}"
+        self.unmanaged_dirs: str = ids.switch_id(switch=SwitchEnum.unmanaged_dirs)
+        self.unmanaged_dirs_q: str = f"#{self.unmanaged_dirs}"
+        self.unwanted: str = ids.switch_id(switch=SwitchEnum.unwanted)
+        self.unwanted_q: str = f"#{self.unwanted}"
 
 
 class ViewIds:
@@ -291,26 +303,26 @@ class ViewIds:
     def __init__(self, ids: AppIds):
 
         # Help tab
-        self.add_help = ids.view_id(view=ViewName.add_help_view)
-        self.add_help_q = f"#{self.add_help}"
-        self.apply_help = ids.view_id(view=ViewName.apply_help_view)
-        self.apply_help_q = f"#{self.apply_help}"
-        self.re_add_help = ids.view_id(view=ViewName.re_add_help_view)
-        self.re_add_help_q = f"#{self.re_add_help}"
-        self.diagram = ids.view_id(view=ViewName.diagram_view)
-        self.diagram_q = f"#{self.diagram}"
+        self.add_help: str = ids.view_id(view=ViewName.add_help_view)
+        self.add_help_q: str = f"#{self.add_help}"
+        self.apply_help: str = ids.view_id(view=ViewName.apply_help_view)
+        self.apply_help_q: str = f"#{self.apply_help}"
+        self.re_add_help: str = ids.view_id(view=ViewName.re_add_help_view)
+        self.re_add_help_q: str = f"#{self.re_add_help}"
+        self.diagram: str = ids.view_id(view=ViewName.diagram_view)
+        self.diagram_q: str = f"#{self.diagram}"
 
         # Config tab
-        self.cat_config = ids.view_id(view=ViewName.cat_config_view)
-        self.cat_config_q = f"#{self.cat_config}"
-        self.ignored = ids.view_id(view=ViewName.git_ignored_view)
-        self.ignored_q = f"#{self.ignored}"
+        self.cat_config: str = ids.view_id(view=ViewName.cat_config_view)
+        self.cat_config_q: str = f"#{self.cat_config}"
+        self.ignored: str = ids.view_id(view=ViewName.git_ignored_view)
+        self.ignored_q: str = f"#{self.ignored}"
 
         # Views or shared across canvases
-        self.pw_mgr_info = ids.view_id(view=ViewName.pw_mgr_info_view)
-        self.pw_mgr_info_q = f"#{self.pw_mgr_info}"
-        self.template_data = ids.view_id(view=ViewName.template_data_view)
-        self.template_data_q = f"#{self.template_data}"
+        self.pw_mgr_info: str = ids.view_id(view=ViewName.pw_mgr_info_view)
+        self.pw_mgr_info_q: str = f"#{self.pw_mgr_info}"
+        self.template_data: str = ids.view_id(view=ViewName.template_data_view)
+        self.template_data_q: str = f"#{self.template_data}"
 
 
 class CanvasIds:
