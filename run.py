3c989b0bbe91048bb141d2f0800ab85e1dc3dfac
matmaer <22435003+matmaer@users.noreply.github.com>
2026-07-02 22:21:46 +0200

    Entry point for pyinstaller build

--- run.py
+++ run.py
@@ -1,10 +1,10 @@
 import sys
 from pathlib import Path
 
-# Add the src folder to Python's import path
+# Add the src folder to python path
 sys.path.insert(0, str(Path(__file__).resolve().parent / "src"))
 
-from chezmoi_mousse.main import run_app
+from chezmoi_mousse.main import run_app  # pyright: ignore[reportMissingTypeStubs]
 
 if __name__ == "__main__":
     run_app()
