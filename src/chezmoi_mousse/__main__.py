5f3a1cf4fdc9925738318e8c919e4e4256f2260e
matmaer <22435003+matmaer@users.noreply.github.com>
2026-07-03 02:09:02 +0200

    Add nuitka-project config.

--- src/chezmoi_mousse/__main__.py
+++ src/chezmoi_mousse/__main__.py
@@ -1,3 +1,11 @@
+# ruff: noqa: E501
+# nuitka-project: --standalone
+# nuitka-project: --onefile
+# nuitka-project: --assume-yes-for-downloads
+# nuitka-project: --output-filename=chezmoi-mousse
+# nuitka-project: --include-data-file=src/chezmoi_mousse/gui.tcss=chezmoi_mousse/gui.tcss
+# nuitka-project: --include-package-data=textual
+
 from .main import run_app
 
 if __name__ == "__main__":
