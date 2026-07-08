8188a449306f5910fb7de3ad512679759a2546b6
matmaer <22435003+matmaer@users.noreply.github.com>
2026-07-08 14:50:15 +0200

    Change function names to better reflect the purpose.

--- static_tests/_test_utils.py
+++ static_tests/_test_utils.py
@@ -10,19 +10,19 @@ MODULE_DIR = Path("src", "chezmoi_mousse")
 
 
 @cache
-def get_module_paths() -> list[Path]:
+def get_file_paths() -> tuple[Path, ...]:
     # This works because pytest sets the cwd to the project root.
-    return list(MODULE_DIR.rglob("*.py"))
+    return tuple(MODULE_DIR.rglob("*.py"))
 
 
 @cache
 def ast_parse(py_file: Path) -> ast.AST:
-    return ast.parse(py_file.read_text())
+    return ast.parse(py_file.read_text(encoding="utf-8"))
 
 
 @cache
 def get_gui_module_paths() -> list[Path]:
-    all_modules = get_module_paths()
+    all_modules = get_file_paths()
     gui_paths = [p for p in all_modules if "gui" in p.parts]
     debug_paths = [p for p in all_modules if "debug" in p.parts]
     app_path = [p for p in all_modules if "_textual_app.py" in p.parts]
@@ -42,7 +42,7 @@ def get_module_ast_class_defs(module_path: Path) -> list[ast.ClassDef]:
 @cache
 def get_modules_importing_class(class_name: str) -> set[Path]:
     modules: set[Path] = set()
-    for module_path in get_module_paths():
+    for module_path in get_file_paths():
         for node in ast.walk(ast_parse(module_path)):
             if isinstance(node, ast.ImportFrom) and class_name in (
                 alias.name for alias in node.names
