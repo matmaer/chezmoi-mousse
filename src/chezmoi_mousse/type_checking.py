97d1a598ea2b26d39e1d4b201d6f8965a46dde79
matmaer <22435003+matmaer@users.noreply.github.com>
2026-07-18 04:42:49 +0200

    deleted:    src/chezmoi_mousse/app_data.py renamed:    src/chezmoi_mousse/_app_ids.py -> src/chezmoi_mousse/app_ids.py renamed:    src/chezmoi_mousse/_run_cmd.py -> src/chezmoi_mousse/run_cmd.py renamed:    src/chezmoi_mousse/_str_enums.py -> src/chezmoi_mousse/str_enums.py new file:   src/chezmoi_mousse/type_checking.py

---
+++ src/chezmoi_mousse/type_checking.py
@@ -0,0 +1,23 @@
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from pathlib import Path
+    from typing import Any
+
+    from chezmoi_mousse.app_ids import AppIds
+    from chezmoi_mousse.app_state import TabIds
+    from chezmoi_mousse.str_enums import StatusCode, TabLabel
+    from chezmoi_mousse.textual_app import ChezmoiGui
+
+    type ParsedJson = dict[str, Any]
+    type StatusDict = dict[Path, StatusCode]
+
+    __all__ = (
+        "AppIds",
+        "ChezmoiGui",
+        "ParsedJson",
+        "StatusCode",
+        "StatusDict",
+        "TabIds",
+        "TabLabel",
+    )
