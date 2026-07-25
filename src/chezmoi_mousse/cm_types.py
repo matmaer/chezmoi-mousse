2e2c41a098cba019ba017aef828cd19e4c426cb4
matmaer <22435003+matmaer@users.noreply.github.com>
2026-07-25 07:25:22 +0200

    Correct NameError: name 'NamedTuple' is not defined

--- src/chezmoi_mousse/cm_types.py
+++ src/chezmoi_mousse/cm_types.py
@@ -1,10 +1,10 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, ClassVar, NamedTuple
 
 if TYPE_CHECKING:
     from pathlib import Path
-    from typing import Any, ClassVar, NamedTuple
+    from typing import Any
 
     from chezmoi_mousse.app_ids import AppIds
     from chezmoi_mousse.cm_command import CommandResult
