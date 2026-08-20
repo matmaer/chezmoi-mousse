f363a070dead6a942fe70c0a599b6b03a8ac1811
matmaer <22435003+matmaer@users.noreply.github.com>
2026-08-20 12:54:10 +0200

    new file:   src/chezmoi_mousse/gui/common/components.py

---
+++ src/chezmoi_mousse/gui/common/components.py
@@ -0,0 +1,55 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from textual.containers import Container
+from textual.widgets import Label, Static
+
+from chezmoi_mousse.str_enums import SectionLabel, Tcss
+
+if TYPE_CHECKING:
+    pass
+
+__all__ = [
+    "CatConfigStatic",
+    "MainSectionLabel",
+    "FlatSectionLabel",
+    "SubSectionLabel",
+]
+
+# Label subclasses
+
+
+class MainSectionLabel(Label):
+    def __init__(self) -> None:
+        super().__init__(SectionLabel.not_set, classes=Tcss.main_section_label)
+
+
+class FlatSectionLabel(Label):
+    def __init__(self) -> None:
+        super().__init__(SectionLabel.not_set, classes=Tcss.flat_section_label)
+
+
+class SubSectionLabel(Label):
+    def __init__(self) -> None:
+        super().__init__(SectionLabel.not_set, classes=Tcss.sub_section_label)
+
+
+# Static subclasses
+
+
+class CatConfigStatic(Static): ...
+
+
+class DiffLineStatic(Static): ...
+
+
+class InfoStatic(Static):
+    def __init__(self, text: str = "") -> None:
+        super().__init__(text, classes=Tcss.info)
+
+
+# Container subclasses
+
+
+class DiffLinesContainer(Container): ...
